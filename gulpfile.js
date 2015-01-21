25808af5253398dc0e5307e3c941deb3c800c9a4
simon <ihmig@kaliber5.de>
2015-01-22 00:08:05 +0100

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('docs', ['docs:publish']);
+
+gulp.task('docs:readme', function (cb) {
+    return gulp.src('README.md')
+        .pipe(gulp.dest('docs'));
 });
 
-gulp.task('deploy', function () {
+
+gulp.task('docs:generate', function (cb) {
+    var exec = require('child_process').exec;
+    exec('ember yuidoc', function (err, stdout, stderr) {
+        cb(err);
+    });
+});
+
+gulp.task('docs:publish', ['docs:generate','docs:readme'], function () {
     return gulp.src('./docs/**/*')
-        .pipe(deploy(options));
+        .pipe(deploy({
+
+        }));
 });
