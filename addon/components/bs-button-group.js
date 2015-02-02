31352ae56d5d8b2f1ebfd9da547a3ea82e2e5d84
simon <ihmig@kaliber5.de>
2015-02-02 01:50:19 +0100

    radio and checkbox types for button-group component

--- addon/components/bs-button-group.js
+++ addon/components/bs-button-group.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
+import Button from 'ember-bootstrap/components/bs-button';
 import SizeClass from 'ember-bootstrap/mixins/size-class';
+
+function initType() {
+    if (this.get('type') === 'radio' || this.get('type') === 'checkbox') {
+        // set all child buttons to toggle
+        this.get('childButtons').forEach(function(button) {
+            button.set('toggle', true);
+        });
+    }
+}
 
 
 /**
@@ -69,6 +79,120 @@ export default Ember.Component.extend(SizeClass, {
      * @default false
      * @public
      */
-    justified: false
+    justified: false,
+
+    /**
+     * The type of the button group specifies how child buttons behave and how the `value` property will be computed:
+     *
+     * ### null
+     * If `type` is not set (null), the button group will add no functionality besides Bootstrap styling
+     *
+     * ### radio
+     * if `type` is set to "radio", the buttons will behave like radio buttons:
+     * * the buttons will toggle (`toggle` property of the child buttons will be set to true)
+     * * only one button may be active
+     * * the `value` property of the button group will reflect the `value` property of the active button
+     *
+     * ### checkbox
+     * if `type` is set to "checkbox", the buttons will behave like checkboxes:
+     * * the buttons will toggle (`toggle` property of the child buttons will be set to true)
+     * * any number of buttons may be active
+     * * the `value` property of the button group will be an array containing the `value` properties of all active buttons
+     *
+     * @property type
+     * @type string
+     * @default null
+     * @public
+     */
+    type: null,
+
+
+    /**
+     * The value of the button group, computed by its child buttons.
+     * See the {{#crossLink "Button-Group/type:attribute"}}`type` property{{/crossLink}} for how the value property is constructed.
+     *
+     * When you set the value, the corresponding buttons will be activated:
+     * * use a single value for a radio button group to activate the button with the same value
+     * * use an aray of values for a checkbox button group to activate all the buttons with values contained in the array
+     *
+     * @property value
+     * @type array|any
+     * @public
+     */
+    value: Ember.computed('activeChildren.@each.value','type',function(key, value){
+        if (arguments.length>1) {
+            var values = !Ember.isArray(value) ? [value] : value;
+            this.get('childButtons')
+                .forEach(function(button) {
+                    button.set('active', values.contains(button.get('value')));
+                });
+            return value;
+        }
+        switch (this.get('type')) {
+            case 'radio':
+                return this.get('activeChildren.firstObject.value');
+                break;
+            case 'checkbox':
+                return this.get('activeChildren').mapBy('value');
+        }
+        return 1;
+    }),
+
+    /**
+     * Array of all child buttons (instances of Bootstrap.Button)
+     * @property childButtons
+     * @type array
+     * @protected
+     */
+    childButtons: Ember.computed.filter('childViews', function(view) {
+        return view instanceof Button;
+    }),
+
+
+    /**
+     * Child buttons that are active (pressed)
+     * @property activeChildren
+     * @type array
+     * @protected
+     */
+    activeChildren: Ember.computed.filterBy('childButtons', 'active', true),
+
+
+    lastActiveChildren: [],
+    newActiveChildren: Ember.computed.setDiff('activeChildren','lastActiveChildren'),
+    _observeButtons: Ember.observer('activeChildren.[]','type', function() {
+        if (this.get('type') !== 'radio') {
+            return;
+        }
+
+        Ember.run.scheduleOnce('actions',this, function(){
+            // the button that just became active
+            var newActive = this.get('newActiveChildren.firstObject');
+
+            if (newActive) {
+                this.beginPropertyChanges();
+                this.get('childButtons').forEach(function(button){
+                    if (button !== newActive) {
+                        button.set('active', false);
+                    }
+                });
+
+                this.endPropertyChanges();
+            }
+            // remember activeChildren, used as a replacement for a before observer as they will be deprecated in the future...
+            this.set('lastActiveChildren', this.get('activeChildren').slice());
+        });
+    }),
+
+    _observeType: Ember.observer('type','childButtons.[]', initType),
+//    _initType: Ember.on('init', initType),
+
+    init: function() {
+        this._super();
+        this.get('activeChildren');
+    }
+
+
+
 
 });
--- tests/unit/components/bs-button-group-test.js
+++ tests/unit/components/bs-button-group-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import {
   moduleForComponent,
   test
@@ -5,7 +6,7 @@ import {
 
 moduleForComponent('bs-button-group', 'BsButtonGroupComponent', {
   // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:bs-button', 'template:components/bs-button']
 });
 
 test('it renders', function() {
@@ -32,4 +33,90 @@ test('button group has correct CSS classes', function() {
     equal(this.$().hasClass('btn-group-lg'),true, 'has size class');
     equal(this.$().hasClass('btn-group-vertical'),true, 'has vertical class');
     equal(this.$().hasClass('btn-group-justified'),true, 'has justified class');
+});
+
+
+test('radio button group has value of selected button', function() {
+    var template = '{{#bs-button value=1}}1{{/bs-button}}{{#bs-button value=2}}2{{/bs-button}}{{#bs-button value=3}}3{{/bs-button}}',
+        component = this.subject({
+            template: Ember.Handlebars.compile(template),
+            type: 'radio'
+        });
+
+    for (var i=0;i<3;i++) {
+        this.$().find('button').eq(i).click();
+        equal(component.get('value'), i+1, 'value of button group must match that of clicked button');
+
+        // check button's active property
+        for (var k=0;k<3;k++) {
+            equal(component.get('childButtons').objectAt(k).get('active'), i===k, 'only clicked button is active');
+        }
+    }
+});
+
+
+test('checkbox button group has value of all selected button', function() {
+    var template = '{{#bs-button value=1}}1{{/bs-button}}{{#bs-button value=2}}2{{/bs-button}}{{#bs-button value=3}}3{{/bs-button}}',
+        component = this.subject({
+            template: Ember.Handlebars.compile(template),
+            type: 'checkbox'
+        }),
+        expectedValue = [];
+
+    for (var i=0;i<3;i++) {
+        this.$().find('button').eq(i).click();
+        expectedValue.push(i+1);
+        deepEqual(component.get('value'), expectedValue, 'value of button group must match array of values of all active buttons');
+
+        // check button's active property
+        for (var k=0;k<3;k++) {
+            equal(component.get('childButtons').objectAt(k).get('active'), k<=i, 'every clicked button is active');
+        }
+    }
+});
+
+
+test('setting radio button group value activates button with same value', function() {
+    var template = '{{#bs-button value=1}}1{{/bs-button}}{{#bs-button value=2}}2{{/bs-button}}{{#bs-button value=3}}3{{/bs-button}}',
+        component = this.subject({
+            template: Ember.Handlebars.compile(template),
+            type: 'radio'
+        });
+
+    this.append();
+
+    for (var i=0;i<3;i++) {
+        Ember.run(function(){
+            component.set('value', i+1);
+        });
+        equal(component.get('value'), i+1, 'value must match set value');
+
+        // check button's active property
+        for (var k=0;k<3;k++) {
+            equal(component.get('childButtons').objectAt(k).get('active'), i===k, 'only button with same value is active');
+        }
+    }
+});
+
+
+
+test('setting checkbox button group value with array of values activates buttons with same value', function() {
+    var template = '{{#bs-button value=1}}1{{/bs-button}}{{#bs-button value=2}}2{{/bs-button}}{{#bs-button value=3}}3{{/bs-button}}',
+        component = this.subject({
+            template: Ember.Handlebars.compile(template),
+            type: 'checkbox'
+        });
+
+    this.append();
+
+    var value = [1,3];
+    Ember.run(function(){
+        component.set('value', value);
+    });
+    deepEqual(component.get('value'), value, 'value must match set value');
+
+    // check button's active property
+    for (var k=0;k<3;k++) {
+        equal(component.get('childButtons').objectAt(k).get('active'), value.contains(k+1), 'only buttons with value contained in set value is active');
+    }
 });
