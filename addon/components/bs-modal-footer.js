0f887ec376d0f7a785e568bf8e8c76689e58e06a
simon <ihmig@kaliber5.de>
2015-02-22 16:52:00 +0100

    modal-footer is a form, instead of having a form modal-footer supports submitButton without custom block template, with optionally disabling it

--- addon/components/bs-modal-footer.js
+++ addon/components/bs-modal-footer.js
@@ -3,17 +3,20 @@ import ModalCloser from 'ember-bootstrap/mixins/modal-closer';
 
 
 export default Ember.Component.extend(ModalCloser, {
+    tagName: 'form',
     classNames: ['modal-footer'],
 
-    buttonTitle: 'Ok',
+    closeTitle: 'Ok',
+    submitTitle: null,
+    hasSubmitButton: Ember.computed.notEmpty('submitTitle'),
+    submitDisabled: false,
 
-    actions: {
-        submit: function() {
-            // send to parent bs-modal component
-            this.triggerAction({
-                action: 'submit'
-            });
-        }
+    submit: function(e) {
+        e.preventDefault();
+        // send to parent bs-modal component
+        this.triggerAction({
+            action: 'submit'
+        });
     }
 
 
