a550118fef620701f22073e797313ba8b4eaab1a
simon <ihmig@kaliber5.de>
2015-03-10 00:17:08 +0100

    Updates

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Bootstrap.Alert",
         "Bootstrap.Button",
         "Bootstrap.Button-Group",
         "Bootstrap.SizeClass",
