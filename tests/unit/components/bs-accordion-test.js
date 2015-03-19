059f71fad335c36f96360bc14047700ac5807c3b
simon <ihmig@kaliber5.de>
2015-03-19 21:18:04 +0100

    fixed ember-cli 0.2.0 / ember 1.10 related accordion errors

--- tests/unit/components/bs-accordion-test.js
+++ tests/unit/components/bs-accordion-test.js
@@ -8,14 +8,14 @@ moduleForComponent('bs-accordion', 'BsAccordionComponent', {
   // needs: ['component:foo', 'helper:bar']
 });
 
-test('it renders', function() {
-  expect(2);
+test('it renders', function(assert) {
+    assert.expect(2);
 
-  // creates the component instance
-  var component = this.subject();
-  equal(component._state, 'preRender');
+    // creates the component instance
+    var component = this.subject();
+    assert.equal(component._state, 'preRender');
 
-  // appends the component to the page
-  this.append();
-  equal(component._state, 'inDOM');
+    // appends the component to the page
+    this.append();
+    assert.equal(component._state, 'inDOM');
 });
