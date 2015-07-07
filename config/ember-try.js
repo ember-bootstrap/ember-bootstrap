2b8a97277d38f10f9c4fcf2a0d1cb3f38f09fa1a
pavankumar <pavankumar.kalikiri@gmail.com>
2015-07-07 09:12:06 +0530

    fixed Travis CI build error.
    https://travis-ci.org/kaliber5/ember-bootstrap/builds/69021265

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,41 +4,33 @@ module.exports = {
       name: 'default',
       dependencies: { }
     },
-    {
-      name: 'ember-v1.12',
-      dependencies: {
-        'ember': 'components/ember#1.12.0'
-      },
-      resolutions: {
-        'ember': '1.12.0'
-      }
-    },
-    {
-      name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
-      }
-    },
-    {
-      name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
-    },
-    {
-      name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
-    }
+    // ,
+    // {
+    //   name: 'ember-release',
+    //   dependencies: {
+    //     'ember': 'components/ember#release'
+    //   },
+    //   resolutions: {
+    //     'ember': 'release'
+    //   }
+    // },
+    // {
+    //   name: 'ember-beta',
+    //   dependencies: {
+    //     'ember': 'components/ember#beta'
+    //   },
+    //   resolutions: {
+    //     'ember': 'beta'
+    //   }
+    // },
+    // {
+    //   name: 'ember-canary',
+    //   dependencies: {
+    //     'ember': 'components/ember#canary'
+    //   },
+    //   resolutions: {
+    //     'ember': 'canary'
+    //   }
+    // }
   ]
 };
