bd7810e7f72f58a6e21a2e79f01e2680eaf70a50
simon <ihmig@kaliber5.de>
2015-08-09 22:18:10 +0200

    fixed jshint

--- tests/integration/components/bs-form-test.js
+++ tests/integration/components/bs-form-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('bs-form', 'Integration | Component | bs-form', {
     integration: true
