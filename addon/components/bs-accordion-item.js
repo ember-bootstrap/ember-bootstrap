f2bbaca82e384df8c49c17e4923cd2b93cac2e92
simon <ihmig@kaliber5.de>
2015-09-09 17:59:22 +0200

    feat(Accordion): added accordion components

--- addon/components/bs-accordion-item.js
+++ addon/components/bs-accordion-item.js
@@ -3,134 +3,166 @@ import TypeClass from 'ember-bootstrap/mixins/type-class';
 import SubComponent from 'ember-bootstrap/mixins/sub-component';
 import I18nSupport from 'ember-bootstrap/mixins/i18n-support';
 
+/**
+ A collapsible/expandable item within an accordion
+
+ See {{#crossLink "Components.Accordion"}}{{/crossLink}} for examples.
+
+
+ @class AccordionItem
+ @namespace Components
+ @extends Ember.Component
+ @uses Mixins.TypeClass
+ @uses Mixins.SubComponent
+ @uses Mixins.I18nSupport
+ */
 export default Ember.Component.extend(TypeClass, SubComponent, I18nSupport, {
-    classNames: ['panel'],
+  classNames: ['panel'],
 
-    /**
-     * @property classTypePrefix
-     * @type String
-     * @default 'panel'
-     * @protected
-     */
-    classTypePrefix: 'panel',
+  /**
+   * @property classTypePrefix
+   * @type String
+   * @default 'panel'
+   * @protected
+   */
+  classTypePrefix: 'panel',
 
-    type: 'default',
+  //type: 'default',
 
-    active: false,
+  /**
+   * True if this item is expanded
+   *
+   * @property active
+   * @protected
+   */
+  active: false,
 
-    title: null,
+  /**
+   * The title of the accordion item, displayed as a .panel-title element
+   *
+   * @property title
+   * @type string
+   * @public
+   */
+  title: null,
 
-    value: Ember.computed.oneWay('elementId'),
+  /**
+   * The value of the accordion item, which is used as the value of the `selected` property of the parent {{#crossLink "Components.Accordion"}}{{/crossLink}} component
+   *
+   * @property value
+   * @public
+   */
+  value: Ember.computed.oneWay('elementId'),
 
-    collapse: Ember.computed.not('transitioning'),
-    collapsing: Ember.computed.alias('transitioning'),
-    in: Ember.computed.and('collapse','active'),
-    collapsed: Ember.computed.not('active'),
-    transitioning: false,
-    collapseHeight: null,
-    collapseStyle: Ember.computed('collapseHeight', function(){
-        var height = this.get('collapseHeight');
-        return height !== null ? Ember.String.htmlSafe('height: %@px'.fmt(height)) : null;
-    }),
+  collapse: Ember.computed.not('transitioning'),
+  collapsing: Ember.computed.alias('transitioning'),
+  in: Ember.computed.and('collapse', 'active'),
+  collapsed: Ember.computed.not('active'),
+  transitioning: false,
+  collapseHeight: null,
+  collapseStyle: Ember.computed('collapseHeight', function () {
+    var height = parseFloat(this.get('collapseHeight'));
+    return new Ember.Handlebars.SafeString(height !== null ? `height: ${height}px` : '');
+  }),
 
-    selected: Ember.computed.alias('parentView.selected'),
+  selected: Ember.computed.alias('parentView.selected'),
 
-    action: 'selected',
+  action: 'selected',
 
 
-    show: function() {
-        var collapseElement = this.$().find('.panel-collapse');
-        var complete = function () {
-            this.setProperties({
-                transitioning: false,
-                collapseHeight: null
-            });
-        };
+  show: function () {
+    var collapseElement = this.$().find('.panel-collapse');
+    var complete = function () {
+      this.setProperties({
+        transitioning: false,
+        collapseHeight: null
+      });
+    };
 
-        this.setProperties({
-            transitioning: true,
-            collapseHeight: 0,
-            active: true
-        });
+    this.setProperties({
+      transitioning: true,
+      collapseHeight: 0,
+      active: true
+    });
 
-        if (!Ember.$.support.transition) {
-            return complete.call(this);
-        }
+    if (!Ember.$.support.transition) {
+      return complete.call(this);
+    }
 
-        collapseElement
-            .one('bsTransitionEnd', Ember.run.bind(this, complete))
-            // @todo: make duration configurable
-            .emulateTransitionEnd(350)
-        ;
+    collapseElement
+      .one('bsTransitionEnd', Ember.run.bind(this, complete))
+      // @todo: make duration configurable
+      .emulateTransitionEnd(350)
+    ;
 
-        Ember.run.next(this, function(){
-            this.set('collapseHeight', collapseElement[0].scrollHeight);
-        });
-    },
+    Ember.run.next(this, function () {
+      this.set('collapseHeight', collapseElement[0].scrollHeight);
+    });
+  },
 
-    hide: function() {
+  hide: function () {
 
-        var collapseElement = this.$().find('.panel-collapse');
-        var complete = function () {
-            this.setProperties({
-                transitioning: false,
-                collapseHeight: null
-            });
-        };
+    var collapseElement = this.$().find('.panel-collapse');
+    var complete = function () {
+      this.setProperties({
+        transitioning: false,
+        collapseHeight: null
+      });
+    };
 
-        this.setProperties({
-            transitioning: true,
-            collapseHeight: collapseElement[0].offsetHeight,
-            active: false
-        });
+    this.setProperties({
+      transitioning: true,
+      collapseHeight: collapseElement[0].offsetHeight,
+      active: false
+    });
 
-        if (!Ember.$.support.transition) {
-            return complete.call(this);
-        }
+    if (!Ember.$.support.transition) {
+      return complete.call(this);
+    }
 
-        collapseElement
-            .one('bsTransitionEnd', Ember.run.bind(this, complete))
-            // @todo: make duration configurable
-            .emulateTransitionEnd(350)
-        ;
+    collapseElement
+      .one('bsTransitionEnd', Ember.run.bind(this, complete))
+      // @todo: make duration configurable
+      .emulateTransitionEnd(350)
+    ;
 
-        Ember.run.next(this, function(){
-            this.set('collapseHeight', 0);
-        });
-    },
+    Ember.run.next(this, function () {
+      this.set('collapseHeight', 0);
+    });
+  },
 
-    actions: {
-        toggleActive: function() {
-            var value = this.get('value'),
-                previous = this.get('selected'),
-                active = this.get('active');
-            if (!active) {
-                this.set('selected', value);
-                this.sendAction('action', value, previous);
-            }
-            else {
-                this.set('selected', null);
-                this.sendAction('action', null, previous);
-            }
-        }
-    },
+  actions: {
+    toggleActive: function () {
+      var value = this.get('value'),
+        previous = this.get('selected'),
+        active = this.get('active');
+      if (!active) {
+        this.set('selected', value);
+        this.sendAction('action', value, previous);
+      }
+      else {
+        this.set('selected', null);
+        this.sendAction('action', null, previous);
+      }
+    }
+  },
 
-    _onSelectedChange: Ember.observer('selected', function() {
-        var selected = this.get('selected'),
-            value = this.get('value'),
-            active = this.get('active');
-        if (selected === value && !active) {
-            this.show();
-        }
-        else if (active) {
-            this.hide();
-        }
-    }),
-    _onInit: Ember.on('init', function(){
-        if (this.get('selected') === this.get('value')) {
-            this.set('active', true);
-        }
-    })
+  _onSelectedChange: Ember.observer('selected', function () {
+    var selected = this.get('selected'),
+      value = this.get('value'),
+      active = this.get('active');
+    if (selected === value && !active) {
+      this.show();
+    }
+    else if (active) {
+      this.hide();
+    }
+  }),
+  _onInit: Ember.on('init', function () {
+    if (this.get('selected') === this.get('value')) {
+      this.set('active', true);
+    }
+  })
 
 
 });
---
+++ tests/integration/components/bs-accordion-test.js
@@ -0,0 +1,46 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+
+moduleForComponent('bs-accordion', 'Integration | Component | bs-accordion', {
+  integration: true
+});
+
+
+test('accordion has correct default markup', function(assert) {
+  this.render(hbs`{{#bs-accordion}}{{#bs-accordion-item value=1 title="TITLE1"}}CONTENT1{{/bs-accordion-item}}{{#bs-accordion-item value=2 title="TITLE2"}}CONTENT2{{/bs-accordion-item}}{{/bs-accordion}}`);
+  assert.equal(this.$(':first-child').hasClass('panel-group'), true, 'accordion has panel-group class');
+});
+
+test('accordion with preselected item has this item expanded', function(assert) {
+  this.set('selected', 1);
+  this.render(hbs`{{#bs-accordion selected=selected}}{{#bs-accordion-item id="firstItem" value=1 title="TITLE1"}}CONTENT1{{/bs-accordion-item}}{{#bs-accordion-item id="secondItem" value=2 title="TITLE2"}}CONTENT2{{/bs-accordion-item}}{{/bs-accordion}}`);
+  var item = this.$('#firstItem');
+
+  assert.equal(item.find('.panel-heading').hasClass('collapsed'), false, 'panel-heading has not collapsed class');
+  assert.equal(item.find('.panel-collapse').hasClass('collapse'), true, 'panel-collapse has collapse class');
+  assert.equal(item.find('.panel-collapse').hasClass('in'), true, 'panel-collapse has in class');
+});
+
+test('changing selected item expands this item', function(assert) {
+  this.set('selected', 1);
+  this.render(hbs`{{#bs-accordion selected=selected}}{{#bs-accordion-item id="firstItem" value=1 title="TITLE1"}}CONTENT1{{/bs-accordion-item}}{{#bs-accordion-item id="secondItem" value=2 title="TITLE2"}}CONTENT2{{/bs-accordion-item}}{{/bs-accordion}}`);
+  this.set('selected', 2);
+
+  var item = this.$('#secondItem');
+  var done = assert.async();
+
+  // wait for transitions to complete
+  setTimeout(() => {
+    assert.equal(item.find('.panel-heading').hasClass('collapsed'), false, 'panel-heading has not collapsed class');
+    assert.equal(item.find('.panel-collapse').hasClass('collapse'), true, 'panel-collapse has collapse class');
+    assert.equal(item.find('.panel-collapse').hasClass('in'), true, 'panel-collapse has in class');
+
+    done();
+  }, 500);
+
+
+});
