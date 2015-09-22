58b0d868c58c2c5f29f6dcc99e1eccb357280b9e
simon <ihmig@kaliber5.de>
2015-09-22 16:08:29 +0200

    feat(AccordionItem): refactored bs-accordion-item to use bs-collapse internally

--- addon/components/bs-accordion-item.js
+++ addon/components/bs-accordion-item.js
@@ -27,16 +27,6 @@ export default Ember.Component.extend(TypeClass, SubComponent, I18nSupport, {
    */
   classTypePrefix: 'panel',
 
-  //type: 'default',
-
-  /**
-   * True if this item is expanded
-   *
-   * @property active
-   * @protected
-   */
-  active: false,
-
   /**
    * The title of the accordion item, displayed as a .panel-title element
    *
@@ -54,82 +44,13 @@ export default Ember.Component.extend(TypeClass, SubComponent, I18nSupport, {
    */
   value: Ember.computed.oneWay('elementId'),
 
-  collapse: Ember.computed.not('transitioning'),
-  collapsing: Ember.computed.alias('transitioning'),
-  in: Ember.computed.and('collapse', 'active'),
-  collapsed: Ember.computed.not('active'),
-  transitioning: false,
-  collapseHeight: null,
-  collapseStyle: Ember.computed('collapseHeight', function () {
-    var height = parseFloat(this.get('collapseHeight'));
-    return new Ember.Handlebars.SafeString(height !== null ? `height: ${height}px` : '');
-  }),
-
   selected: Ember.computed.alias('parentView.selected'),
 
+  collapsed: Ember.computed('value','selected', function() {
+    return this.get('value') !== this.get('selected');
+  }),
+
   action: 'selected',
-
-
-  show: function () {
-    var collapseElement = this.$().find('.panel-collapse');
-    var complete = function () {
-      this.setProperties({
-        transitioning: false,
-        collapseHeight: null
-      });
-    };
-
-    this.setProperties({
-      transitioning: true,
-      collapseHeight: 0,
-      active: true
-    });
-
-    if (!Ember.$.support.transition) {
-      return complete.call(this);
-    }
-
-    collapseElement
-      .one('bsTransitionEnd', Ember.run.bind(this, complete))
-      // @todo: make duration configurable
-      .emulateTransitionEnd(350)
-    ;
-
-    Ember.run.next(this, function () {
-      this.set('collapseHeight', collapseElement[0].scrollHeight);
-    });
-  },
-
-  hide: function () {
-
-    var collapseElement = this.$().find('.panel-collapse');
-    var complete = function () {
-      this.setProperties({
-        transitioning: false,
-        collapseHeight: null
-      });
-    };
-
-    this.setProperties({
-      transitioning: true,
-      collapseHeight: collapseElement[0].offsetHeight,
-      active: false
-    });
-
-    if (!Ember.$.support.transition) {
-      return complete.call(this);
-    }
-
-    collapseElement
-      .one('bsTransitionEnd', Ember.run.bind(this, complete))
-      // @todo: make duration configurable
-      .emulateTransitionEnd(350)
-    ;
-
-    Ember.run.next(this, function () {
-      this.set('collapseHeight', 0);
-    });
-  },
 
   actions: {
     toggleActive: function () {
@@ -145,24 +66,6 @@ export default Ember.Component.extend(TypeClass, SubComponent, I18nSupport, {
         this.sendAction('action', null, previous);
       }
     }
-  },
-
-  _onSelectedChange: Ember.observer('selected', function () {
-    var selected = this.get('selected'),
-      value = this.get('value'),
-      active = this.get('active');
-    if (selected === value && !active) {
-      this.show();
-    }
-    else if (active) {
-      this.hide();
-    }
-  }),
-  _onInit: Ember.on('init', function () {
-    if (this.get('selected') === this.get('value')) {
-      this.set('active', true);
-    }
-  })
-
+  }
 
 });
