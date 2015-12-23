06b13030c55d4e4067e08cfd90f203340862caa8
Bastien Cecchinato <bcecchinato@users.noreply.github.com>
2015-12-23 13:46:27 +0100

    Adding the possibility to use multiple for select

--- addon/components/bs-select.js
+++ addon/components/bs-select.js
@@ -18,6 +18,7 @@ import I18nSupport from 'ember-bootstrap/mixins/i18n-support';
 export default Ember.Component.extend(I18nSupport, {
   tagName: 'select',
   classNames: ['form-control'],
+  attributeBindings: ['multiple'],
 
   content: null,
   prompt: null,
@@ -34,27 +35,39 @@ export default Ember.Component.extend(I18nSupport, {
     }
   },
 
-  change: function() {
+  change: function () {
     var selectEl = this.$().get(0);
-    var selectedIndex = selectEl.selectedIndex;
+
+    let selection = this.get('multiple') ? [] : {};
+    let hasPrompt = !!this.get('prompt');
     var content = this.get('content');
 
-    // decrement index by 1 if we have a prompt
-    var hasPrompt = !!this.get('prompt');
-    var contentIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
+    if (this.get('multiple')) {
+      var selectedIndexes = selectEl.selectedOptions;
 
-    var selection = undefined;
-    if(content.constructor && content.constructor === Array){
-      selection = content[contentIndex];
+      for (var i = 0; i < selectedIndexes.length; i++) {
+        let contentIndex = hasPrompt ? selectedIndexes.item(i).index - 1 : selectedIndexes.item(i).index;
+        if (content.constructor && content.constructor === Array) {
+          selection.push(content[contentIndex]);
+        } else {
+          selection.push(content.objectAt(contentIndex));
+        }
+      }
     } else {
-      selection = content.objectAt(contentIndex);
+      let selectedIndex = selectEl.selectedIndex;
+      let contentIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
+      if (content.constructor && content.constructor === Array) {
+        selection = content[contentIndex];
+      } else {
+        selection = content.objectAt(contentIndex);
+      }
     }
 
     // set the local, shadowed selection to avoid leaking
     // changes to `selection` out via 2-way binding
     this.set('value', selection);
 
-    if(this.get('action')){
+    if (this.get('action')) {
       this.sendAction('action', selection);
     }
   }
