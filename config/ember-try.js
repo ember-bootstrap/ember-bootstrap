0cc19d0b1910e82e99a98a6e414806c815012cf2
simon <ihmig@kaliber5.de>
2015-12-29 14:46:12 +0100

    Added ember 2.0/2.1 to emby-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,6 +5,14 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-2.0',
+      dependencies: { 'ember': '~2.0.0' }
+    },
+    {
+      name: 'ember-2.1',
+      dependencies: { 'ember': '~2.1.0' }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
