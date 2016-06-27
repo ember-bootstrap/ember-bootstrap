f9aa6a7328c965a9b3a056e9f358e260ed9347d0
simon <ihmig@kaliber5.de>
2016-06-28 01:02:19 +0200

    contains helper test

--- addon/helpers/bs-contains.js
+++ addon/helpers/bs-contains.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
-const { isArray } = Ember;
+const { isArray, A } = Ember;
 
 export function bsContains(params/*, hash*/) {
-  return isArray(params[0]) ? params[0].contains(params[1]) : false;
+  return isArray(params[0]) ? A(params[0]).contains(params[1]) : false;
 }
 
 export default Ember.Helper.helper(bsContains);
