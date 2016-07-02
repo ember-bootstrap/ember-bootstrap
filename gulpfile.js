5d0d3f2123669b9a896497da81637402710d1ee4
simon <ihmig@kaliber5.de>
2016-07-02 02:10:47 +0200

    chore(gulp): copy changelog with stripped html

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,39 @@ var gulp = require('gulp');
 var ghpages = require('gh-pages');
 var path = require('path');
 var file = require('gulp-file');
+var striptags = require('striptags')
+var transform = require('gulp-transform');
 
 gulp.task('docs', ['docs:publish']);
 
 
 gulp.task('docs:generate', function (cb) {
-    var exec = require('child_process').exec;
-    exec('ember ember-cli-yuidoc', function (err, stdout, stderr) {
-        cb(err);
-    });
+  var exec = require('child_process').exec;
+  exec('ember ember-cli-yuidoc', function (err, stdout, stderr) {
+    cb(err);
+  });
 });
 
 gulp.task('docs:copyChangelog', function () {
-    return gulp.src('CHANGELOG.md')
-        .pipe(gulp.dest('docs'))
+  return gulp.src('CHANGELOG.md')
+    // jekyll seems to have a problem with mixed html/markdown
+    // so strip away html for docs
+    .pipe(transform(striptags, {encoding: 'utf8'}))
+    .pipe(gulp.dest('docs'))
 });
 
-gulp.task('docs:publish', ['docs:generate','docs:copyChangelog'], function () {
-    ghpages.publish(path.join(__dirname, 'docs'), {
-        src: '{api/**/*,CHANGELOG.md}',
-        add: true
-    });
+gulp.task('docs:publish', ['docs:generate', 'docs:copyChangelog'], function () {
+  ghpages.publish(path.join(__dirname, 'docs'), {
+    src: '{api/**/*,CHANGELOG.md}',
+    add: true
+  });
 });
 
 gulp.task('docs:changelog', function (done) {
-    require('conventional-changelog')({
-    }, function(err, log) {
-        file('CHANGELOG.md', log, { src: true })
-            .pipe(gulp.dest('./'))
-            .on('end', done);
-    });
+  require('conventional-changelog')({
+  }, function (err, log) {
+    file('CHANGELOG.md', log, { src: true })
+      .pipe(gulp.dest('./'))
+      .on('end', done);
+  });
 });
