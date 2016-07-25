7763bf2690d265019bff48c049863bec26faf8c6
Stephen Vance <steve@vance.com>
2016-07-25 00:08:38 -0400

    Start to create bs-navbar*. Got basic functionality working but lots of better ways, tests, and docs to go.

---
+++ addon/components/bs-navbar-header.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import layout from '../templates/components/bs-navbar-header';
+
+export default Ember.Component.extend({
+  layout,
+
+  classNames: ['navbar-header']
+});
