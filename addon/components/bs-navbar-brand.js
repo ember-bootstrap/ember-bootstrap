dcc3c67b7976cdf2d9ee1908ccd82f8dbebcd96e
Stephen Vance <steve@vance.com>
2016-08-06 18:32:09 -0400

    Add navbar-brand docs.

--- addon/components/bs-navbar-brand.js
+++ addon/components/bs-navbar-brand.js
@@ -1,11 +1,30 @@
 import Ember from 'ember';
 import layout from '../templates/components/bs-navbar-brand';
 
+/**
+ * Component for the brand within a [Components.Navbar](Components.Navbar.html) component. Have a look there for examples.
+ *
+ * This implements `.navbar-brand` [element behavior](http://getbootstrap.com/components/#navbar-brand-image).
+ *
+ * @class NavbarBrand
+ * @namespace Components
+ * @extends Ember.Component
+ * @public
+ */
 export default Ember.Component.extend({
   layout,
 
   tagName: 'a',
   classNames: ['navbar-brand'],
   attributeBindings: ['href'],
+
+  /**
+   * The target of the brand link.
+   *
+   * @property href
+   * @type String
+   * @default '#'
+   * @public
+   */
   href: '#'
 });
