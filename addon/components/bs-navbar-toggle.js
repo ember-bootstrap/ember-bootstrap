54a2363c53f49a5956822a271867639ff2bfc847
Stephen Vance <steve@vance.com>
2016-08-06 18:52:31 -0400

    Add navbar-toggle docs and make the toggle property readonly.

--- addon/components/bs-navbar-toggle.js
+++ addon/components/bs-navbar-toggle.js
@@ -1,9 +1,31 @@
-import layout from '../templates/components/bs-navbar-toggle';
+import Ember from 'ember';
 import BsButtonComponent from 'ember-bootstrap/components/bs-button';
+import layout from '../templates/components/bs-navbar-toggle';
 
+/**
+ * Component to implement the responsive menu toggle behavior in a [Components.Navbar](Components.Navbar.html)
+ * component. Have a look there for examples.
+ *
+ * @class NavbarToggle
+ * @namespace Components
+ * @extends Components.Button
+ * @public
+ */
 export default BsButtonComponent.extend({
   layout,
 
   classNames: ['navbar-toggle', 'collapsed'],
-  toggle: true
+
+  /**
+   * See [Components.Button](Components.Button.html).
+   *
+   * @property toggle
+   * @type Boolean
+   * @default true
+   * @readonly
+   * @protected
+   */
+  toggle: Ember.computed(function() {
+    return true;
+  })
 });
