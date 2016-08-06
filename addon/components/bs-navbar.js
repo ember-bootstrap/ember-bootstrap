b31ae94c250d2d7b134d908926ddca61f6fcd140
Stephen Vance <steve@vance.com>
2016-08-06 18:08:35 -0400

    Use the type-class mixin to handle navbar-default/inverse behavior.

--- addon/components/bs-navbar.js
+++ addon/components/bs-navbar.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
+import TypeClass from 'ember-bootstrap/mixins/type-class';
 import layout from '../templates/components/bs-navbar';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(TypeClass, {
   layout,
 
   tagName: 'nav',
-  classNames: ['navbar', 'navbar-default'],
+  classNames: ['navbar'],
+
+  classTypePrefix: 'navbar',
 
   navbarCollapsed: true,
 
