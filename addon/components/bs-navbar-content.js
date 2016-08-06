ab15f54e6103fa872df721394fd4dc4f5d3034ce
Stephen Vance <steve@vance.com>
2016-08-06 17:27:57 -0400

    Create navbar content component derived from collapse.

---
+++ addon/components/bs-navbar-content.js
@@ -0,0 +1,8 @@
+import layout from '../templates/components/bs-navbar-content';
+import BsCollapseComponent from 'ember-bootstrap/components/bs-collapse';
+
+export default BsCollapseComponent.extend({
+  layout,
+
+  classNames: ['navbar-collapse']
+});
