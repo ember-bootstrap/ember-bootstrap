6d3ddf6358022839249d0f03f34df0741e09c7da
Stephen Vance <steve@vance.com>
2016-08-06 18:33:22 -0400

    Add navbar-nav docs.

--- addon/components/bs-navbar-nav.js
+++ addon/components/bs-navbar-nav.js
@@ -1,11 +1,31 @@
+import Ember from 'ember';
 import BsNavComponent from 'ember-bootstrap/components/bs-nav';
 import layout from '../templates/components/bs-navbar-nav';
 
+/**
+ * Component for the `.nav` element within a [Components.Navbar](Components.Navbar.html)
+ * component. Have a look there for examples.
+ *
+ * @class NavbarNav
+ * @namespace Components
+ * @extends Components.Nav
+ * @public
+ */
 export default BsNavComponent.extend({
   layout,
 
   classNames: ['navbar-nav'],
 
-  // TODO: How do I enforce this?
-  justified: false
+  /**
+   * Per [the bootstrap docs](http://getbootstrap.com/components/#navbar),
+   * justified navbar nav links are not supported.
+   *
+   * @property justified
+   * @type boolean
+   * @default false
+   * @final
+   */
+  justified: Ember.computed(function() {
+    return false;
+  })
 });
