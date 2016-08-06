21700b0ff0946be00d6e09bdcc30cd242c81ae70
Stephen Vance <steve@vance.com>
2016-08-06 16:46:10 -0400

    Get the navbar really working properly with the toggle action and the collapsed state. Need to improve the ergonomics, but it's working correctly now.

--- tests/dummy/app/controllers/navbars.js
+++ tests/dummy/app/controllers/navbars.js
@@ -1,17 +1,5 @@
 import Ember from 'ember';
 
-const { computed, A } = Ember;
-
 export default Ember.Controller.extend({
-  type: computed.oneWay('typeChoices.firstObject'),
-  stacked: false,
-  justified: false,
-  typeChoices: A([
-    {
-      id: 'pills'
-    },
-    {
-      id: 'tabs'
-    }
-  ])
+  nbe: false
 });
