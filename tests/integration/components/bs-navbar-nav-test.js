ecb87a179cf98ad9ec615a937fcc0bb6d4e5ccc2
Stephen Vance <steve@vance.com>
2016-08-07 15:34:08 -0400

    Write the navbar nav integration test.

--- tests/integration/components/bs-navbar-nav-test.js
+++ tests/integration/components/bs-navbar-nav-test.js
@@ -1,14 +1,11 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('bs-navbar-nav', 'Integration | Component | bs navbar nav', {
+moduleForComponent('bs-navbar-nav', 'Integration | Component | bs-navbar-nav', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{bs-navbar-nav}}`);
 
   assert.equal(this.$().text().trim(), '');
@@ -21,4 +18,22 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('it has correct markup', function(assert) {
+  this.render(hbs`
+    {{#bs-navbar-nav}}
+      Nav
+    {{/bs-navbar-nav}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'Nav');
+  assert.equal(this.$('.navbar-nav').length, 1, 'there is only one element with the navbar-nav class');
+  assert.ok(this.$('.navbar-nav').hasClass('nav'), 'it has the nav class indicating it derives from bs-nav');
+});
+
+test('it no longer supports the justified option', function(assert) {
+  this.render(hbs`{{bs-navbar-nav justified=true}}`);
+
+  assert.equal(this.$('.navbar-justified').length, 0, 'the justified class was not applied');
 });
