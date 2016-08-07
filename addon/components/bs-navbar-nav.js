ffda055c02340b3c72e5c15eb2727afb460e3dbf
Stephen Vance <steve@vance.com>
2016-08-07 16:33:05 -0400

    Fix the read-only problem with justified, although I don't understand why the test didn't catch it.

--- addon/components/bs-navbar-nav.js
+++ addon/components/bs-navbar-nav.js
@@ -6,6 +6,9 @@ import layout from '../templates/components/bs-navbar-nav';
  * Component for the `.nav` element within a [Components.Navbar](Components.Navbar.html)
  * component. Have a look there for examples.
  *
+ * Per [the bootstrap docs](http://getbootstrap.com/components/#navbar),
+ * justified navbar nav links are not supported.
+ *
  * @class NavbarNav
  * @namespace Components
  * @extends Components.Nav
@@ -16,16 +19,8 @@ export default BsNavComponent.extend({
 
   classNames: ['navbar-nav'],
 
-  /**
-   * Per [the bootstrap docs](http://getbootstrap.com/components/#navbar),
-   * justified navbar nav links are not supported.
-   *
-   * @property justified
-   * @type boolean
-   * @default false
-   * @final
-   */
-  justified: Ember.computed(function() {
-    return false;
-  })
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('justified', false);
+  }
 });
