ec99eb9a3948fcf28f788028fccb82cbdf2414ec
Stephen Vance <steve@vance.com>
2016-08-06 20:34:55 -0400

    Add navbar-header docs.

--- addon/components/bs-navbar-header.js
+++ addon/components/bs-navbar-header.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/bs-navbar-header';
 
+/**
+ * Component to wrap the header section of a [Components.Navbar](Components.Navbar.html) component.
+ * Have a look there for examples.
+ *
+ * @class NavbarHeader
+ * @namespace Components
+ * @extends Ember.Component
+ * @public
+ */
 export default Ember.Component.extend({
   layout,
 
