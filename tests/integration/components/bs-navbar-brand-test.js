db3922cb037d354001843e44fe0be42d22065b52
Stephen Vance <steve@vance.com>
2016-08-07 15:15:01 -0400

    Fix test naming to match the rest of the addon.

--- tests/integration/components/bs-navbar-brand-test.js
+++ tests/integration/components/bs-navbar-brand-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('bs-navbar-brand', 'Integration | Component | bs navbar brand', {
+moduleForComponent('bs-navbar-brand', 'Integration | Component | bs-navbar-brand', {
   integration: true
 });
 
