ae4d2af0969f3ab7322793d385dc28177dc58acb
simon <ihmig@kaliber5.de>
2016-11-23 00:17:23 +0100

    feat(Dropdown): refactored to make use of contextual components, added onShow and onHide actions

--- tests/integration/components/bs-dropdown-test.js
+++ tests/integration/components/bs-dropdown-test.js
@@ -1,4 +1,5 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('bs-dropdown', 'Integration | Component | bs-dropdown', {
@@ -18,18 +19,28 @@ test('dropdown container supports dropup style', function(assert) {
 });
 
 test('dropdown container with dropdown button has btn-group class', function(assert) {
-  this.render(hbs`{{#bs-dropdown}}{{#bs-dropdown-button}}Dropdown <span class="caret"></span>{{/bs-dropdown-button}}{{#bs-dropdown-menu}}<li><a href="#">Something</a></li>{{/bs-dropdown-menu}}{{/bs-dropdown}}`);
-  assert.equal(this.$(':first-child').hasClass('btn-group'), true, 'has btn-group class');
+  this.render(hbs`{{#bs-dropdown as |dd|}}{{#dd.button}}Dropdown <span class="caret"></span>{{/dd.button}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
+  let done = assert.async();
+  // timeout is needed as class is set in next run loop
+  setTimeout(() => {
+    assert.equal(this.$(':first-child').hasClass('btn-group'), true, 'has btn-group class');
+    done();
+  }, 0);
 });
 
 test('dropdown container with dropdown button supports dropup style', function(assert) {
-  this.render(hbs`{{#bs-dropdown direction="up"}}{{#bs-dropdown-button}}Dropdown <span class="caret"></span>{{/bs-dropdown-button}}{{#bs-dropdown-menu}}<li><a href="#">Something</a></li>{{/bs-dropdown-menu}}{{/bs-dropdown}}`);
-  assert.equal(this.$(':first-child').hasClass('btn-group'), true, 'has btn-group class');
-  assert.equal(this.$(':first-child').hasClass('dropup'), true, 'has dropup class');
+  this.render(hbs`{{#bs-dropdown direction="up" as |dd|}}{{#dd.button}}Dropdown <span class="caret"></span>{{/dd.button}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
+  let done = assert.async();
+  // timeout is needed as class is set in next run loop
+  setTimeout(() => {
+    assert.equal(this.$(':first-child').hasClass('btn-group'), true, 'has btn-group class');
+    assert.equal(this.$(':first-child').hasClass('dropup'), true, 'has dropup class');
+    done();
+  }, 0);
 });
 
 test('dropdown-toggle toggles dropdown visibility', function(assert) {
-  this.render(hbs`{{#bs-dropdown}}{{#bs-dropdown-toggle}}Dropdown <span class="caret"></span>{{/bs-dropdown-toggle}}{{#bs-dropdown-menu}}<li><a href="#">Something</a></li>{{/bs-dropdown-menu}}{{/bs-dropdown}}`);
+  this.render(hbs`{{#bs-dropdown as |dd|}}{{#dd.toggle}}Dropdown <span class="caret"></span>{{/dd.toggle}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
 
   assert.equal(this.$(':first-child').hasClass('open'), false, 'has not open class');
   this.$('a.dropdown-toggle').click();
@@ -39,7 +50,7 @@ test('dropdown-toggle toggles dropdown visibility', function(assert) {
 });
 
 test('opened dropdown will close on outside click', function(assert) {
-  this.render(hbs`{{#bs-dropdown}}{{#bs-dropdown-toggle}}Dropdown <span class="caret"></span>{{/bs-dropdown-toggle}}{{#bs-dropdown-menu}}<li><a href="#">Something</a></li>{{/bs-dropdown-menu}}{{/bs-dropdown}}`);
+  this.render(hbs`{{#bs-dropdown as |dd|}}{{#dd.toggle}}Dropdown <span class="caret"></span>{{/dd.toggle}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
 
   this.$('a.dropdown-toggle').click();
   assert.equal(this.$(':first-child').hasClass('open'), true, 'has open class');
@@ -49,7 +60,7 @@ test('opened dropdown will close on outside click', function(assert) {
 });
 
 test('clicking dropdown menu will close it', function(assert) {
-  this.render(hbs`{{#bs-dropdown}}{{#bs-dropdown-toggle}}Dropdown <span class="caret"></span>{{/bs-dropdown-toggle}}{{#bs-dropdown-menu}}<li><a href="#">Something</a></li>{{/bs-dropdown-menu}}{{/bs-dropdown}}`);
+  this.render(hbs`{{#bs-dropdown as |dd|}}{{#dd.toggle}}Dropdown <span class="caret"></span>{{/dd.toggle}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
   this.$('a.dropdown-toggle').click();
   assert.equal(this.$(':first-child').hasClass('open'), true, 'has open class');
 
@@ -58,10 +69,29 @@ test('clicking dropdown menu will close it', function(assert) {
 });
 
 test('clicking dropdown menu when closeOnMenuClick is false will not close it', function(assert) {
-  this.render(hbs`{{#bs-dropdown closeOnMenuClick=false}}{{#bs-dropdown-toggle}}Dropdown <span class="caret"></span>{{/bs-dropdown-toggle}}{{#bs-dropdown-menu}}<li><a href="#">Something</a></li>{{/bs-dropdown-menu}}{{/bs-dropdown}}`);
+  this.render(hbs`{{#bs-dropdown closeOnMenuClick=false as |dd|}}{{#dd.toggle}}Dropdown <span class="caret"></span>{{/dd.toggle}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
   this.$('a.dropdown-toggle').click();
   assert.equal(this.$(':first-child').hasClass('open'), true, 'has open class');
 
   this.$('ul.dropdown-menu a').click();
   assert.equal(this.$(':first-child').hasClass('open'), true, 'has open class');
+});
+
+test('opening dropdown calls onShow action', function(assert) {
+  let action = this.spy();
+  this.on('show', action);
+  this.render(hbs`{{#bs-dropdown onShow=(action "show") as |dd|}}{{#dd.toggle}}Dropdown <span class="caret"></span>{{/dd.toggle}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
+
+  this.$('a.dropdown-toggle').click();
+  assert.ok(action.calledOnce);
+});
+
+test('closing dropdown calls onHide action', function(assert) {
+  let action = this.spy();
+  this.on('hide', action);
+  this.render(hbs`{{#bs-dropdown onHide=(action "hide") as |dd|}}{{#dd.toggle}}Dropdown <span class="caret"></span>{{/dd.toggle}}{{#dd.menu}}<li><a href="#">Something</a></li>{{/dd.menu}}{{/bs-dropdown}}`);
+
+  this.$('a.dropdown-toggle').click();
+  this.$('ul.dropdown-menu a').click();
+  assert.ok(action.calledOnce);
 });
