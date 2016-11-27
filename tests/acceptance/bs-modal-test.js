3a39999530b975efa6df48be74de55701e1daff0
simon <ihmig@kaliber5.de>
2016-11-27 10:20:24 +0100

    test(Acceptance): correctly tear down modal container in acceptance tests
    (cherry picked from commit f2de76a)

--- tests/acceptance/bs-modal-test.js
+++ tests/acceptance/bs-modal-test.js
@@ -1,16 +1,7 @@
-import Ember from 'ember';
-import { module, test } from 'qunit';
-import startApp from '../../tests/helpers/start-app';
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 
-module('Acceptance | bs modal', {
-  beforeEach() {
-    this.application = startApp();
-  },
-
-  afterEach() {
-    Ember.run(this.application, 'destroy');
-  }
-});
+moduleForAcceptance('Acceptance | bs modal');
 
 // Test for https://github.com/kaliber5/ember-bootstrap/issues/53
 test('Closing modal waits for transition to complete', function(assert) {
