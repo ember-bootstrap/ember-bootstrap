dfa0ae96874326081b4a58e3a24b407c380ab59e
Stephen Vance <steve@vance.com>
2016-12-28 14:40:00 -0500

    Fix the accordion in the dummy app. The selection state was no longer in scope with contextual components.

---
+++ tests/dummy/app/controllers/accordion.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  selected: "1",
+
+  actions: {
+    select(selected) {
+      this.set('selected', selected);
+    }
+  }
+});
