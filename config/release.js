468f60a3c5d6f288748aa5859a604c704c0a626f
simonihmig <ihmig@kaliber5.de>
2017-01-09 00:54:21 +0100

    changelog prompt

--- config/release.js
+++ config/release.js
@@ -1,6 +1,22 @@
 /* jshint node:true */
 var RSVP = require('rsvp');
 
+function promptForCompletion(project) {
+  return project.ui.prompt({
+    type: 'confirm',
+    name: 'answer',
+    message: '[Changelog] Enter "y" when you have finished making any desired modifications to CHANGELOG.md',
+    choices: [
+      { key: 'y', name: 'Yes, update', value: 'yes' },
+      { key: 'n', name: 'No, cancel', value: 'no' }
+    ]
+  }).then(function(response) {
+    if (!response.answer) {
+      return promptForCompletion(project);
+    }
+  });
+}
+
 // For details on each option run `ember help release`
 module.exports = {
   manifest: [ 'package.json', 'bower.json', 'yuidoc.json' ],
@@ -8,6 +24,14 @@ module.exports = {
     var gulp = require('gulp');
     require('../gulpfile.js');
     var start = RSVP.denodeify(gulp.start.bind(gulp));
-    return start('changelog');
+
+    project.ui.startProgress('Collecting changes for CHANGELOG.md...');
+    return start('changelog')
+      .then(function() {
+        project.ui.stopProgress();
+        return promptForCompletion(project);
+      });
   }
 };
+
+
