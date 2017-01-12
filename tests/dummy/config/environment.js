2f18dddb899c805fa5608651a1b76a57fea1b89a
simonihmig <ihmig@kaliber5.de>
2017-01-13 00:04:22 +0100

    Fix dummy config

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 
 module.exports = function(environment) {
-  let ENV = {
+  var ENV = {
     modulePrefix: 'dummy',
-    environment,
+    environment: environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
