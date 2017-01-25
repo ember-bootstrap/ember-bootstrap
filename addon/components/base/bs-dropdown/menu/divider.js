87393379231382c8253e2a193c86d3293ba34d2e
Stephen Vance <steve@vance.com>
2017-01-25 14:33:23 -0500

    Add dropdown divider and modify the demo to use it.

---
+++ addon/components/base/bs-dropdown/menu/divider.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+/**
+ Component for a dropdown menu divider.
+
+ See [Components.Dropdown](Components.Dropdown.html) for examples.
+
+ @class DropdownMenuDivider
+ @namespace Components
+ @extends Ember.Component
+ @public
+ */
+export default Ember.Component.extend({});
---
+++ addon/components/bs4/bs-dropdown/menu/divider.js
@@ -0,0 +1,5 @@
+import Divider from 'ember-bootstrap/components/base/bs-dropdown/menu/divider';
+
+export default Divider.extend({
+  classNames: ['dropdown-divider']
+});
