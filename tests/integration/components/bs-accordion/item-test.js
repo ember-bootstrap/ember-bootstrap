afeedd10960f308ce76617c31e88d5984ad2d9db
Stephen Vance <steve@vance.com>
2017-01-31 10:02:08 -0500

    Fix some of the accordion tests and segregate others. This needs revisiting to finish.

--- tests/integration/components/bs-accordion/item-test.js
+++ tests/integration/components/bs-accordion/item-test.js
@@ -2,13 +2,13 @@ import {
   moduleForComponent
 } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import test from 'ember-sinon-qunit/test-support/test';
+import { testBS3, testBS4 } from '../../../helpers/bootstrap-test';
 
 moduleForComponent('bs-accordion-item', 'Integration | Component | bs-accordion-item', {
   integration: true
 });
 
-test('accordion item has correct default markup', function(assert) {
+testBS3('accordion item has correct default markup', function(assert) {
   this.render(hbs`{{#bs-accordion/item title="TITLE"}}CONTENT{{/bs-accordion/item}}`);
   assert.equal(this.$(':first-child').hasClass('panel'), true, 'has panel class');
   assert.equal(this.$(':first-child').hasClass('panel-default'), true, 'has panel-default class');
@@ -19,11 +19,31 @@ test('accordion item has correct default markup', function(assert) {
   assert.equal(this.$('.panel-body').text().trim(), 'CONTENT', 'panel-body has correct content');
 });
 
-test('calls onClick action when clicking heading', function(assert) {
+testBS4('accordion item has correct default markup', function(assert) {
+  this.render(hbs`{{#bs-accordion/item title="TITLE"}}CONTENT{{/bs-accordion/item}}`);
+  assert.equal(this.$(':first-child').hasClass('card'), true, 'has card class');
+  assert.equal(this.$(':first-child').hasClass('card-default'), true, 'has card-default class');
+  assert.equal(this.$('.card-heading').hasClass('collapsed'), true, 'card-heading has collapsed class');
+  assert.equal(this.$('.card-collapse').hasClass('collapse'), true, 'card-collapse has collapse class');
+  assert.equal(this.$('.card-collapse').hasClass('show'), false, 'card-collapse has not show class');
+  assert.equal(this.$('.card-title').text().trim(), 'TITLE', 'card-title has correct title');
+  assert.equal(this.$('.card-body').text().trim(), 'CONTENT', 'card-body has correct content');
+});
+
+testBS3('calls onClick action when clicking heading', function(assert) {
   let action = this.spy();
   this.on('click', action);
   this.render(hbs`{{#bs-accordion/item value=1 onClick=(action "click") title="TITLE"}}CONTENT{{/bs-accordion/item}}`);
 
   this.$('.panel-heading').click();
+  assert.ok(action.calledWith(1), 'onClick action has been called.');
+});
+
+testBS4('calls onClick action when clicking heading', function(assert) {
+  let action = this.spy();
+  this.on('click', action);
+  this.render(hbs`{{#bs-accordion/item value=1 onClick=(action "click") title="TITLE"}}CONTENT{{/bs-accordion/item}}`);
+
+  this.$('.card-heading').click();
   assert.ok(action.calledWith(1), 'onClick action has been called.');
 });
