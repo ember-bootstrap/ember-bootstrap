5760f1d7dbdb5664a93936a297e1e04d4adef0a1
Stephen Vance <steve@vance.com>
2017-01-31 11:29:16 -0500

    Fix navbar tests for BS4.

--- tests/integration/components/bs-navbar/toggle-test.js
+++ tests/integration/components/bs-navbar/toggle-test.js
@@ -1,4 +1,5 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent } from 'ember-qunit';
+import { test, testBS3, testBS4 } from '../../../helpers/bootstrap-test';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('bs-navbar/toggle', 'Integration | Component | bs-navbar/toggle', {
@@ -20,11 +21,20 @@ test('it renders', function(assert) {
   assert.equal(this.$().text().trim(), 'template block text');
 });
 
-test('it has correct markup', function(assert) {
+testBS3('it has correct markup', function(assert) {
   this.render(hbs`{{bs-navbar/toggle}}`);
 
   assert.equal(this.$('button').length, 1, 'there is exactly one button element');
   assert.ok(this.$('button').hasClass('navbar-toggle'), 'the toggle has the navbar-toggle class');
+  assert.ok(this.$('button').hasClass('collapsed'), 'the toggle has the collapsed class');
+  assert.ok(this.$('button').hasClass('btn'), 'the toggle has the btn class indicating it derives from bs-button');
+});
+
+testBS4('it has correct markup', function(assert) {
+  this.render(hbs`{{bs-navbar/toggle}}`);
+
+  assert.equal(this.$('button').length, 1, 'there is exactly one button element');
+  assert.ok(this.$('button').hasClass('navbar-toggler'), 'the toggle has the navbar-toggler class');
   assert.ok(this.$('button').hasClass('collapsed'), 'the toggle has the collapsed class');
   assert.ok(this.$('button').hasClass('btn'), 'the toggle has the btn class indicating it derives from bs-button');
 });
