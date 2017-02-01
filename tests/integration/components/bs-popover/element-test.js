934d143fb67ad52f0c5a5837248a876bae4d3f26
Stephen Vance <steve@vance.com>
2017-02-01 18:13:47 -0500

    Apply test helpers to popover element tests.

--- tests/integration/components/bs-popover/element-test.js
+++ tests/integration/components/bs-popover/element-test.js
@@ -1,12 +1,12 @@
 import { moduleForComponent } from 'ember-qunit';
-import { test, testBS3, testBS4 } from '../../../helpers/bootstrap-test';
+import { placementClassFor, test, visibilityClass } from '../../../helpers/bootstrap-test';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('bs-popover/element', 'Integration | Component | bs-popover/element', {
   integration: true
 });
 
-testBS3('it has correct markup', function(assert) {
+test('it has correct markup', function(assert) {
   // Template block usage:
   this.render(hbs`
     {{#bs-popover/element fade=true in=true title="dummy title"}}
@@ -15,32 +15,15 @@ testBS3('it has correct markup', function(assert) {
   `);
 
   assert.ok(this.$(':first-child').hasClass('popover'), 'has popover class');
-  assert.ok(this.$(':first-child').hasClass('top'), 'has top class');
+  assert.ok(this.$(':first-child').hasClass(placementClassFor('popover', 'top')), 'has placement class');
   assert.ok(this.$(':first-child').hasClass('fade'), 'has fade class');
-  assert.ok(this.$(':first-child').hasClass('in'), 'has in class');
+  assert.ok(this.$(':first-child').hasClass(visibilityClass()), 'has visibility class');
   assert.equal(this.$('.arrow').length, 1, 'has arrow');
   assert.equal(this.$('.popover-title').text().trim(), 'dummy title', 'shows title');
   assert.equal(this.$('.popover-content').text().trim(), 'template block text', 'shows content');
 });
 
-testBS4('it has correct markup', function(assert) {
-  // Template block usage:
-  this.render(hbs`
-    {{#bs-popover/element fade=true in=true title="dummy title"}}
-      template block text
-    {{/bs-popover/element}}
-  `);
-
-  assert.ok(this.$(':first-child').hasClass('popover'), 'has popover class');
-  assert.ok(this.$(':first-child').hasClass('popover-top'), 'has popover-top class');
-  assert.ok(this.$(':first-child').hasClass('fade'), 'has fade class');
-  assert.ok(this.$(':first-child').hasClass('show'), 'has show class');
-  assert.equal(this.$('.arrow').length, 1, 'has arrow');
-  assert.equal(this.$('.popover-title').text().trim(), 'dummy title', 'shows title');
-  assert.equal(this.$('.popover-content').text().trim(), 'template block text', 'shows content');
-});
-
-testBS3('it supports different placements', function(assert) {
+test('it supports different placements', function(assert) {
   let placements = ['top', 'left', 'bottom', 'right'];
   this.render(hbs`
     {{#bs-popover/element placement=placement title="dummy title"}}
@@ -49,21 +32,8 @@ testBS3('it supports different placements', function(assert) {
   `);
   placements.forEach((placement) => {
     this.set('placement', placement);
-    assert.ok(this.$(':first-child').hasClass(placement), `has ${placement} class`);
-  });
-});
-
-testBS4('it supports different placements', function(assert) {
-  let placements = ['top', 'left', 'bottom', 'right'];
-  this.render(hbs`
-    {{#bs-popover/element placement=placement title="dummy title"}}
-      template block text
-    {{/bs-popover/element}}
-  `);
-  placements.forEach((placement) => {
-    this.set('placement', placement);
-    let expectedClass = `popover-${placement}`;
-    assert.ok(this.$(':first-child').hasClass(expectedClass), `has ${expectedClass} class`);
+    let placementClass = placementClassFor('popover', placement);
+    assert.ok(this.$(':first-child').hasClass(placementClass), `has ${placementClass} class`);
   });
 });
 
