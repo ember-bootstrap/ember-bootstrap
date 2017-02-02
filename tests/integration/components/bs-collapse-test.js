1e3cd615fbeb2384f9bad3c69b60cb6275a03cdf
Stephen Vance <steve@vance.com>
2017-02-01 19:24:21 -0500

    Apply test helpers to collapse tests.

--- tests/integration/components/bs-collapse-test.js
+++ tests/integration/components/bs-collapse-test.js
@@ -2,39 +2,26 @@ import {
   moduleForComponent
 } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { test, testBS3, testBS4 } from '../../helpers/bootstrap-test';
+import { test, visibilityClass } from '../../helpers/bootstrap-test';
 
 moduleForComponent('bs-collapse', 'Integration | Component | bs-collapse', {
   integration: true
 });
 
-testBS3('collapse has correct default markup', function(assert) {
+test('collapse has correct default markup', function(assert) {
   this.render(hbs`{{#bs-collapse}}<p>Just some content</p>{{/bs-collapse}}`);
   assert.equal(this.$(':first-child').hasClass('collapse'), true, 'collapse has collapse class');
-  assert.equal(this.$(':first-child').hasClass('in'), false, 'collapse does not have in class');
+  assert.equal(this.$(':first-child').hasClass(visibilityClass()), false, 'collapse does not have visibility class');
 
 });
 
-testBS4('collapse has correct default markup', function(assert) {
-  this.render(hbs`{{#bs-collapse}}<p>Just some content</p>{{/bs-collapse}}`);
-  assert.equal(this.$(':first-child').hasClass('collapse'), true, 'collapse has collapse class');
-  assert.equal(this.$(':first-child').hasClass('show'), false, 'collapse does not have show class');
-
-});
-
-testBS3('expanded collapse has correct default markup', function(assert) {
+test('expanded collapse has correct default markup', function(assert) {
   this.render(hbs`{{#bs-collapse collapsed=false}}<p>Just some content</p>{{/bs-collapse}}`);
   assert.equal(this.$(':first-child').hasClass('collapse'), true, 'collapse has collapse class');
-  assert.equal(this.$(':first-child').hasClass('in'), true, 'collapse has in class');
+  assert.equal(this.$(':first-child').hasClass(visibilityClass()), true, 'collapse has visibility class');
 });
 
-testBS4('expanded collapse has correct default markup', function(assert) {
-  this.render(hbs`{{#bs-collapse collapsed=false}}<p>Just some content</p>{{/bs-collapse}}`);
-  assert.equal(this.$(':first-child').hasClass('collapse'), true, 'collapse has collapse class');
-  assert.equal(this.$(':first-child').hasClass('show'), true, 'collapse has show class');
-});
-
-testBS3('setting collapse to false expands this item', function(assert) {
+test('setting collapse to false expands this item', function(assert) {
   let showAction = this.spy();
   let shownAction = this.spy();
   this.on('show', showAction);
@@ -53,32 +40,7 @@ testBS3('setting collapse to false expands this item', function(assert) {
   setTimeout(() => {
     assert.ok(shownAction.calledOnce, 'onShown action has been called');
     assert.equal(this.$(':first-child').hasClass('collapse'), true, 'collapse has collapse class');
-    assert.equal(this.$(':first-child').hasClass('in'), true, 'collapse has in class');
-
-    done();
-  }, 500);
-});
-
-testBS4('setting collapse to false expands this item', function(assert) {
-  let showAction = this.spy();
-  let shownAction = this.spy();
-  this.on('show', showAction);
-  this.on('shown', shownAction);
-
-  this.set('collapsed', true);
-  this.render(hbs`{{#bs-collapse collapsed=collapsed onShow=(action "show") onShown=(action "shown")}}<p>Just some content</p>{{/bs-collapse}}`);
-  this.set('collapsed', false);
-
-  assert.ok(showAction.calledOnce, 'onShow action has been called');
-  assert.equal(this.$(':first-child').hasClass('collapsing'), true, 'collapse has collapsing class while transition is running');
-
-  let done = assert.async();
-
-  // wait for transitions to complete
-  setTimeout(() => {
-    assert.ok(shownAction.calledOnce, 'onShown action has been called');
-    assert.equal(this.$(':first-child').hasClass('collapse'), true, 'collapse has collapse class');
-    assert.equal(this.$(':first-child').hasClass('show'), true, 'collapse has show class');
+    assert.equal(this.$(':first-child').hasClass(visibilityClass()), true, 'collapse has visibility class');
 
     done();
   }, 500);
