76489e17ffa9c4923dfd04edc7a45ffc7469b982
Stephen Vance <steve@vance.com>
2017-02-06 12:46:06 -0500

    Consolidate navbar documentation.

--- addon/components/base/bs-navbar/toggle.js
+++ addon/components/base/bs-navbar/toggle.js
@@ -5,6 +5,11 @@ import layout from 'ember-bootstrap/templates/components/bs-navbar/toggle';
  * Component to implement the responsive menu toggle behavior in a [Components.Navbar](Components.Navbar.html)
  * component. Have a look there for examples.
  *
+ * ### Bootstrap 3/4 Notes
+ *
+ * The inline version of the component uses the triple `icon-bar` styling for Bootstrap 3 and the
+ * `navbar-toggler-icon` styling for Bootstrap 4.
+ *
  * @class NavbarToggle
  * @namespace Components
  * @extends Components.Button
@@ -15,4 +20,15 @@ export default BsButtonComponent.extend({
 
   classNameBindings: ['collapsed'],
   collapsed: true
+
+  /**
+   * Bootstrap 4 Only: Defines the alignment of the toggler. Valid values are 'left' and 'right'
+   * to set the `navbar-toggler-*` class.
+   *
+   * @property align
+   * @type String
+   * @default null
+   * @public
+   */
+
 });
--- addon/components/bs3/bs-navbar.js
+++ addon/components/bs3/bs-navbar.js
@@ -1,14 +1,1 @@
-import Navbar from 'ember-bootstrap/components/base/bs-navbar';
-
-export default Navbar.extend({
-
-  /**
-   * Controls whether the wrapping div is a fluid container or not.
-   *
-   * @property fluid
-   * @type boolean
-   * @default true
-   * @public
-   */
-  fluid: true
-});
+export { default } from 'ember-bootstrap/components/base/bs-navbar';
