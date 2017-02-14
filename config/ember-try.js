ca512f08b9117808d312331a13211d9908008ca7
Stephen Vance <steve@vance.com>
2017-02-14 17:01:51 -0500

    Add ember-try config to run latest and oldest bs4 scenarios.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,3 +1,33 @@
 /*jshint node:true*/
-module.exports = {
+module.exports = function() {
+  return {
+    command: 'BOOTSTRAPVERSION=3 ember test',
+
+    useVersionCompatibility: true,
+
+    scenarios: [
+      {
+        name: 'bs4 release',
+
+        command: 'BOOTSTRAPVERSION=4 ember test',
+
+        bower: {
+          dependencies: {
+            'ember': 'release'
+          }
+        }
+      },
+      {
+        name: 'bs4 oldest',
+
+        command: 'BOOTSTRAPVERSION=4 ember test',
+
+        bower: {
+          dependencies: {
+            'ember': '2.3.1'
+          }
+        }
+      }
+    ]
+  };
 };
