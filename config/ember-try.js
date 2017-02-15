c64e8155f8927662658ca4868b2cf6e67078c103
Stephen Vance <steve@vance.com>
2017-02-15 08:46:07 -0500

    Expand ember-try config to run lts and beta bs4 scenarios.

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,13 +7,46 @@ module.exports = function() {
 
     scenarios: [
       {
-        name: 'bs4 release',
+        name: 'bs4-beta',
+
+        command: 'BOOTSTRAPVERSION=4 ember test',
+
+        bower: {
+          dependencies: {
+            'ember': 'beta'
+          }
+        }
+      },
+      {
+        name: 'bs4-release',
 
         command: 'BOOTSTRAPVERSION=4 ember test',
 
         bower: {
           dependencies: {
             'ember': 'release'
+          }
+        }
+      },
+      {
+        name: 'bs4-2.8-lts',
+
+        command: 'BOOTSTRAPVERSION=4 ember test',
+
+        bower: {
+          dependencies: {
+            'ember': '2.8.3'
+          }
+        }
+      },
+      {
+        name: 'bs4-2.4-lts',
+
+        command: 'BOOTSTRAPVERSION=4 ember test',
+
+        bower: {
+          dependencies: {
+            'ember': '2.4.6'
           }
         }
       },
