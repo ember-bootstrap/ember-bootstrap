5fb470b3bb1a6ba87a72d4e8f12614bd81fcbc7d
simonihmig <ihmig@kaliber5.de>
2017-02-19 00:39:18 +0100

    Update fastboot tests

--- fastboot-tests/button-test.js
+++ fastboot-tests/button-test.js
@@ -1,14 +1,18 @@
-var expect = require('chai').expect;
+'use strict';
+
+const expect = require('chai').expect;
+const setupTest = require('ember-fastboot-addon-tests').setupTest;
 
 describe('button', function() {
+  setupTest();
 
   it('renders', function() {
     return this.visit('/button')
       .then(function(res) {
-        var $ = res.jQuery;
-        // var response = res.response;
+        let $ = res.jQuery;
+        let response = res.response;
 
-        // add your real tests here
+        expect(response.statusCode).to.equal(200);
         expect($('#button').length).to.equal(1);
         expect($('#button').hasClass('btn')).to.be.true;
         expect($('#button').hasClass('btn-primary')).to.be.true;
--- fastboot-tests/form-test.js
+++ fastboot-tests/form-test.js
@@ -1,14 +1,18 @@
-var expect = require('chai').expect;
+'use strict';
+
+const expect = require('chai').expect;
+const setupTest = require('ember-fastboot-addon-tests').setupTest;
 
 describe('form', function() {
+  setupTest();
 
   it('renders', function() {
     return this.visit('/form')
       .then(function(res) {
-        var $ = res.jQuery;
-        // var response = res.response;
+        let $ = res.jQuery;
+        let response = res.response;
 
-        // add your real tests here
+        expect(response.statusCode).to.equal(200);
         expect($('form#form').length).to.equal(1);
         expect($('form .form-group').length).to.equal(4);
         expect($('form .form-group:eq(0) label').text().trim()).to.equal('Email');
