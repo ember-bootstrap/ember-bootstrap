1faeae9bcc8cb15356564d9ab2d6ecea01856021
Stephen Vance <steve@vance.com>
2017-02-21 14:39:13 -0500

    Get form validation feedback icons working for BS4.

--- addon/components/bs4/bs-form/element/control/input.js
+++ addon/components/bs4/bs-form/element/control/input.js
@@ -1,1 +1,13 @@
-export { default } from 'ember-bootstrap/components/base/bs-form/element/control/input';
+import Ember from 'ember';
+import FormElementControlInput from 'ember-bootstrap/components/base/bs-form/element/control/input';
+
+export default FormElementControlInput.extend({
+  classNameBindings: ['formFeedbackClass'],
+
+  validationType: null,
+
+  formFeedbackClass: Ember.computed('validationType', function() {
+    let validationType = this.get('validationType');
+    return validationType ? `form-control-${validationType}` : null;
+  })
+});
