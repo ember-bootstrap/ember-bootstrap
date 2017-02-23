4549b5839d6bae4b9bd18892c19121746eb04708
Stephen Vance <steve@vance.com>
2017-02-23 16:24:30 -0500

    Bs4 validation feedback (#246)
    * BS4 eliminates `has-feedback` class.
    
    * Minor formatting change.
    
    * Correct the class for the error feedback.
    
    * Get form validation feedback icons working for BS4.

--- addon/components/bs3/bs-form/group.js
+++ addon/components/bs3/bs-form/group.js
@@ -4,6 +4,8 @@ import FormGroup from 'ember-bootstrap/components/base/bs-form/group';
 const { computed } = Ember;
 
 export default FormGroup.extend({
+  classNameBindings: ['hasFeedback'],
+
   classTypePrefix: 'form-group',
 
   _validationType: computed.readOnly('validation')
