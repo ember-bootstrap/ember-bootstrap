0a21d09b9c5642abf9626cb9427b0de6688a4b33
Stephen Vance <steve@vance.com>
2017-03-03 18:22:03 -0500

    Accept the options, sanitize them, and make installation decisions based on that.

--- blueprints/ember-bootstrap/index.js
+++ blueprints/ember-bootstrap/index.js
@@ -7,9 +7,16 @@ const path = require('path');
 const writeFile = rsvp.denodeify(fs.writeFile);
 const chalk = require('chalk');
 const BuildConfigEditor = require('ember-cli-build-config-editor');
+const SilentError = require('silent-error'); // From ember-cli
 
 const bs3Version = '^3.3.7';
 const bs4Version = 'next';
+
+const validPreprocessors = [
+  'none',
+  'less',
+  'sass'
+];
 
 module.exports = {
   name: 'ember-bootstrap',
@@ -17,67 +24,100 @@ module.exports = {
   description: 'Configure ember-bootstrap',
 
   availableOptions: [
-    { name: 'bootstrap-version', type: Number, default: 3 }
+    { name: 'bootstrap-version', type: Number, default: 3, aliases: ['bv'] },
+    { name: 'target-preprocessor', type: String, aliases: ['tp'] }
   ],
+
+  works: 'insideProject',
 
   normalizeEntityName() {
   },
 
+  beforeInstall(option) {
+    let { bootstrapVersion, targetPreprocessor } = option;
+    if (bootstrapVersion !== 3 && bootstrapVersion !== 4) {
+      throw new SilentError('Bootstrap version must be 3 or 4');
+    }
+
+    if (targetPreprocessor && !validPreprocessors.includes(targetPreprocessor)) {
+      throw new SilentError(`Valid preprocessors are: ${validPreprocessors.join(', ')}`);
+    }
+
+    if (!targetPreprocessor) {
+      let dependencies = this.project.dependencies();
+      if ('ember-cli-sass' in dependencies) {
+        targetPreprocessor = option.targetPreprocessor = 'sass';
+      } else if ('ember-cli-less' in dependencies) {
+        targetPreprocessor = option.targetPreprocessor = 'less';
+      } else {
+        targetPreprocessor = 'none';
+      }
+    }
+
+    if (bootstrapVersion === 4 && targetPreprocessor === 'less') {
+      throw new SilentError('You cannot use LESS with Bootstrap 4');
+    }
+
+    this.ui.writeLine(chalk.green(`Installing for Bootstrap ${bootstrapVersion} using preprocessor ${targetPreprocessor}`));
+  },
+
   afterInstall(option) {
     return this.addDependencies(option)
-      .then(() => this.addPreprocessorImport())
+      .then(() => this.addPreprocessorImport(option))
       .then(() => this.addBuildConfiguration(option));
   },
 
   addDependencies(option) {
-    let dependencies = this.project.dependencies();
-
     if (option.bootstrapVersion === 4) {
       return this.addPackageToProject('bootstrap', bs4Version);
     }
 
-    if ('ember-cli-sass' in dependencies) {
+    // bootstrapVersion === 3
+    if (option.targetPreprocessor === 'sass') {
       return this.addPackageToProject('bootstrap-sass', bs3Version);
     }
 
     return this.addBowerPackageToProject('bootstrap', bs3Version);
   },
 
-  addPreprocessorImport() {
-    let dependencies = this.project.dependencies();
-    let type;
+  addPreprocessorImport(option) {
+    let { targetPreprocessor } = option;
     let importStatement = '\n@import "ember-bootstrap/bootstrap";\n';
 
-    if ('ember-cli-sass' in dependencies) {
-      type = 'scss';
-    } else if ('ember-cli-less' in dependencies) {
-      type = 'less';
+    if (targetPreprocessor === 'none') {
+      return;
     }
 
-    if (type) {
-      let stylePath = path.join('app', 'styles');
-      let file = path.join(stylePath, `app.${type}`);
+    let extension = targetPreprocessor === 'sass' ? 'scss' : 'less';
 
-      if (!fs.existsSync(stylePath)) {
-        fs.mkdirSync(stylePath);
-      }
-      if (fs.existsSync(file)) {
-        this.ui.writeLine(chalk.green(`Added import statement to ${file}`));
-        return this.insertIntoFile(file, importStatement, {});
-      } else {
-        this.ui.writeLine(chalk.green(`Created ${file}`));
-        return writeFile(file, importStatement);
-      }
+    let stylePath = path.join('app', 'styles');
+    let file = path.join(stylePath, `app.${extension}`);
+
+    if (!fs.existsSync(stylePath)) {
+      fs.mkdirSync(stylePath);
+    }
+    if (fs.existsSync(file)) {
+      this.ui.writeLine(chalk.green(`Added import statement to ${file}`));
+      return this.insertIntoFile(file, importStatement, {});
+    } else {
+      this.ui.writeLine(chalk.green(`Created ${file}`));
+      return writeFile(file, importStatement);
     }
   },
 
   addBuildConfiguration(option) {
     let file = 'ember-cli-build.js';
+    let { bootstrapVersion, targetPreprocessor } = option;
     let settings = {
-      bootstrapVersion: option.bootstrapVersion
+      bootstrapVersion
     };
-    if (option.bootstrapVersion === 4) {
+
+    if (bootstrapVersion === 4) {
       settings.importBootstrapFont = false;
+    }
+
+    if (targetPreprocessor !== 'none') {
+      settings.importBootstrapCSS = false;
     }
 
     if (fs.existsSync(file)) {
