62cf8cf54d832a9b68bca6442f3c3155e2622221
Stephen Vance <steve@vance.com>
2017-03-22 13:32:08 -0400

    Move the config edited message to only happen after success.

--- blueprints/ember-bootstrap/index.js
+++ blueprints/ember-bootstrap/index.js
@@ -191,13 +191,13 @@ module.exports = {
       return;
     }
 
-    this.ui.writeLine(chalk.green(`Added ember-bootstrap configuration to ${file}`));
     let source = fs.readFileSync(file, 'utf-8');
     let build = new BuildConfigEditor(source);
 
     try {
       let newBuild = build.edit(this.name, settings);
       fs.writeFileSync(file, newBuild.code());
+      this.ui.writeLine(chalk.green(`Added ember-bootstrap configuration to ${file}`));
     } catch(error) {
       let settingsString = JSON.stringify(settings);
       this.ui.writeLine(chalk.red(`Configuration file could not be edited. Manually update your ember-cli-build.js to include '${this.name}': ${settingsString}`));
