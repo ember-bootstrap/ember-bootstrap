424e8a97dd5d195569f7479f727259d4a3ab6748
simonihmig <ihmig@kaliber5.de>
2017-05-04 23:01:35 +0200

    Upgrade to ember-cli 2.13

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,8 +3,10 @@ import {
   setResolver
 } from 'ember-qunit';
 import { settings } from 'ember-native-dom-helpers';
+import { start } from 'ember-cli-qunit';
 
 setResolver(resolver);
 
 // make rootElement of `ember-native-dom-helpers` match `this.$()`, see https://github.com/cibernox/ember-native-dom-helpers/issues/63
 settings.rootElement = '#ember-testing > .ember-view' || settings.rootElement;
+start();
