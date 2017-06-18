8671bb9f2b4e8aeb4f517dc2884bfe2745cccaf0
Christian von Rohr <cv@frontal.ch>
2017-06-18 10:59:30 +0200

    Linting

--- addon/utils/get-position.js
+++ addon/utils/get-position.js
@@ -37,11 +37,10 @@ export default function getPosition(el) {
   return assign({}, clonedRect, scroll, outerDims, elOffset);
 }
 
-
 function getScrollPosition() {
   return {
     //    IE, Firefox                            Chrome
-    top:  document.documentElement.scrollTop  || document.body.scrollTop  || 0,
+    top: document.documentElement.scrollTop || document.body.scrollTop || 0,
     left: document.documentElement.scrollLeft || document.body.scrollLeft || 0
   };
 }
