19dcda00b7e80e9f26e7276beda4389200fd7caf
Caio <c410.f3r@gmail.com>
2017-07-08 21:21:03 -0300

    Wrong icon class name for BS4

--- addon/components/bs4/bs-carousel/controls.js
+++ addon/components/bs4/bs-carousel/controls.js
@@ -2,7 +2,7 @@ import CarouselControls from 'ember-bootstrap/components/base/bs-carousel/contro
 
 export default CarouselControls.extend({
   nextClassName: 'carousel-control-next',
-  nextIcon: 'carousel-control-next',
+  nextIcon: 'carousel-control-next-icon',
   prevClassName: 'carousel-control-prev',
-  prevIcon: 'carousel-control-prev'
+  prevIcon: 'carousel-control-prev-icon'
 });
