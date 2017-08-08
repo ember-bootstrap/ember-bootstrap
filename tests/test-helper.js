fd85f3040aae4a6082714305a02d4bbc201ceec3
simonihmig <ihmig@kaliber5.de>
2017-08-08 21:05:12 +0200

    test(IE11): polyfill Promise for async/await tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import resolver from './helpers/resolver';
 import {
   setResolver
@@ -6,6 +7,10 @@ import { settings } from 'ember-native-dom-helpers';
 import { start } from 'ember-cli-qunit';
 
 setResolver(resolver);
+
+if (typeof Promise === 'undefined') {
+  window.Promise = Ember.RSVP.Promise;
+}
 
 // make rootElement of `ember-native-dom-helpers` match `this.$()`, see https://github.com/cibernox/ember-native-dom-helpers/issues/63
 settings.rootElement = '#ember-testing > .ember-view' || settings.rootElement;
