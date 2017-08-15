fec8dae81a8b5b0d5cefea0ad8be82ddd4f4241b
simonihmig <ihmig@kaliber5.de>
2017-08-15 09:33:54 +0200

    test: test for IE 10 + 9

--- testem.sauce.js
+++ testem.sauce.js
@@ -9,6 +9,8 @@ module.exports = {
     'SLChrome',
     'SLFirefox',
     'SLIE11',
+    'SLIE10',
+    'SLIE9',
     'SLSafari',
     'SLSafariLast'
   ],
@@ -72,6 +74,44 @@ module.exports = {
         'public',
         '-p',
         'Windows 10',
+        '--attach',
+        '--no-connect',
+        '--url'
+      ],
+      'protocol': 'browser'
+    },
+
+    SLIE10: {
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'internet explorer',
+        '-v',
+        '10',
+        '--visibility',
+        'public',
+        '-p',
+        'Windows 8',
+        '--attach',
+        '--no-connect',
+        '--url'
+      ],
+      'protocol': 'browser'
+    },
+
+    SLIE9: {
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'internet explorer',
+        '-v',
+        '9',
+        '--visibility',
+        'public',
+        '-p',
+        'Windows 7',
         '--attach',
         '--no-connect',
         '--url'
