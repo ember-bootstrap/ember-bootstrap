5088191aecdfb3ae386b6c83ce74bcaf44495f8c
Donald Wasserman <djwasserman@gmail.com>
2017-08-18 14:19:05 -0400

    added assert statement

--- addon/components/bs3/bs-form.js
+++ addon/components/bs3/bs-form.js
@@ -1,8 +1,9 @@
-import { computed } from '@ember/object';
+import { computed, assert } from '@ember/object';
 import Form from 'ember-bootstrap/components/base/bs-form';
 
 export default Form.extend({
   layoutClass: computed('formLayout', function() {
+    assert('must provide a `formLayout` attribute.', this.get('formLayout'));
     let layout = this.get('formLayout');
     return layout === 'vertical' ? 'form' : `form-${layout}`;
   }).readOnly()
