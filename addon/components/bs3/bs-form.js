52cdfc2bb249090cac5dd4223b177893cbb75ff1
Donald Wasserman <djwasserman@gmail.com>
2017-08-18 14:24:56 -0400

    a little more fancy

--- addon/components/bs3/bs-form.js
+++ addon/components/bs3/bs-form.js
@@ -3,8 +3,10 @@ import Form from 'ember-bootstrap/components/base/bs-form';
 
 export default Form.extend({
   layoutClass: computed('formLayout', function() {
-    assert('must provide a `formLayout` attribute.', this.get('formLayout'));
     let layout = this.get('formLayout');
+    let supportedTypes = ['vertical', 'horizontal', 'inline'];
+    assert('must provide a valid `formLayout` attribute.',
+      supportedTypes.includes(this.get('formLayout')));
     return layout === 'vertical' ? 'form' : `form-${layout}`;
   }).readOnly()
 });
