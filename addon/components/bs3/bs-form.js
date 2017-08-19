af73949c50d0d66a70d8e35b3f58932429ff9b19
Donald Wasserman <djwasserman@gmail.com>
2017-08-18 22:23:03 -0400

    updated code/tests

--- addon/components/bs3/bs-form.js
+++ addon/components/bs3/bs-form.js
@@ -1,4 +1,6 @@
-import { computed, assert } from '@ember/object';
+import { computed } from '@ember/object';
+import { assert } from '@ember/debug';
+
 import Form from 'ember-bootstrap/components/base/bs-form';
 
 export default Form.extend({
@@ -6,7 +8,7 @@ export default Form.extend({
     let layout = this.get('formLayout');
     let supportedTypes = ['vertical', 'horizontal', 'inline'];
     assert('must provide a valid `formLayout` attribute.',
-      supportedTypes.includes(this.get('formLayout')));
+      (supportedTypes.indexOf(layout) >= 0));
     return layout === 'vertical' ? 'form' : `form-${layout}`;
   }).readOnly()
 });
