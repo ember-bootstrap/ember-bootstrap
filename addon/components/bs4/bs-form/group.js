a12522a03f58ff7997f0696d9f1f8427312143c1
Stephen Vance <steve@vance.com>
2017-08-19 13:04:41 -0400

    Fix inline checkbox styling.

--- addon/components/bs4/bs-form/group.js
+++ addon/components/bs4/bs-form/group.js
@@ -3,7 +3,7 @@ import { computed } from '@ember/object';
 import FormGroup from 'ember-bootstrap/components/base/bs-form/group';
 
 export default FormGroup.extend({
-  classNameBindings: ['isHorizontal:row', 'isCheckbox:form-check:form-group'],
+  classNameBindings: ['isHorizontal:row', 'isCheckbox:form-check:form-group', 'isInlineCheckbox:form-check-inline'],
   classTypePrefix: 'form-control',
 
   /**
@@ -23,6 +23,10 @@ export default FormGroup.extend({
    * @private
    */
   isHorizontal: equal('formLayout', 'horizontal').readOnly(),
+
+  isInline: computed.equal('formLayout', 'inline').readOnly(),
+
+  isInlineCheckbox: computed.and('isCheckbox', 'isInline').readOnly(),
 
   _validationType: computed('validation', function() {
     let validation = this.get('validation');
