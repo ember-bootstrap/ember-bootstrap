764e15e4060c25fbae07aec483109b1f2f8c6e7e
simonihmig <ihmig@kaliber5.de>
2017-10-16 00:48:26 +0200

    Added popper to BS4 dropdown

--- addon/components/bs4/bs-dropdown.js
+++ addon/components/bs4/bs-dropdown.js
@@ -1,5 +1,5 @@
 import DropDown from 'ember-bootstrap/components/base/bs-dropdown';
 
 export default DropDown.extend({
-  classNameBindings: ['inNav:nav-item']
+  classNameBindings: ['inNav:nav-item', 'isOpen:show']
 });
--- addon/components/bs4/bs-dropdown/menu.js
+++ addon/components/bs4/bs-dropdown/menu.js
@@ -1,8 +1,36 @@
 import DropdownMenu from 'ember-bootstrap/components/base/bs-dropdown/menu';
+import { computed } from '@ember/object';
 
 export default DropdownMenu.extend({
-  tagName: 'div',
-  classNameBindings: ['isOpen:show'],
+  tagName: '',
 
-  isOpen: false
+  isOpen: false,
+
+  flip: true,
+
+  popperPlacement: computed('direction', 'align', function() {
+    let placement = 'bottom-start';
+
+    if (this.get('direction') === 'up') {
+      placement = 'top-start';
+      if (this.get('align') === 'right') {
+        placement = 'top-end';
+      }
+    } else if (this.get('align') === 'right') {
+      placement = 'bottom-end';
+    }
+    return placement;
+  }),
+
+  popperModifiers: computed('inNav', 'flip', function() {
+    return {
+      // @todo add offset config
+      applyStyle: {
+        enabled: !this.get('inNav')
+      },
+      flip: {
+        enabled: this.get('flip')
+      }
+    };
+  })
 });
