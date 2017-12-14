f883da4b3fa6f909240eb7d6eb967f8357cf92a5
Bas Kamer <bas@bushbaby.nl>
2017-12-14 13:08:44 +0100

    tests

--- tests/integration/components/bs-form/element/control/input-test.js
+++ tests/integration/components/bs-form/element/control/input-test.js
@@ -21,4 +21,10 @@ module('Integration | Component | bs form/element/control/input', function(hooks
     await render(hbs`{{bs-form/element/control/input size="sm"}}`);
     assert.dom('input').hasClass('form-control-sm', 'input has small class');
   });
+
+  testBS4('support plaintext', async function(assert) {
+    await render(hbs`{{bs-form/element/control/input type="email-plain"}}`);
+    assert.dom('input').hasClass('form-control', 'input has form-control class');
+    assert.dom('input').hasClass('form-control-plaintext', 'input has plaintext class');
+  });
 });
