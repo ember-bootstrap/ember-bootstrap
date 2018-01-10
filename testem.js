7bedcf9d32dc163f25538713fcda0100c8bac3cb
simonihmig <ihmig@kaliber5.de>
2018-01-11 00:17:29 +0100

    test: use --no-sandbox to work around https://github.com/travis-ci/travis-ci/issues/8836

--- testem.js
+++ testem.js
@@ -14,7 +14,8 @@ module.exports = {
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=0',
-        '--window-size=1440,900'
+        '--window-size=1440,900',
+        '--no-sandbox'
       ]
     }
   }
