5991bc0b8c419179dc22a8fa2786c52d5a580188
simonihmig <simon.ihmig@kaliber5.de>
2018-11-18 22:45:08 +0100

    Use lerna-changelog for Changelog generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,18 @@
 /* eslint-env node */
 const gulp = require('gulp');
-const conventionalChangelog = require('gulp-conventional-changelog');
 const ghPages = require('gulp-gh-pages');
-const exec = require('child_process').exec;
+const execa = require('execa');
 const merge = require('merge-stream');
 const striptags = require('striptags');
 const transform = require('gulp-transform');
+const insert = require('gulp-insert');
 
-gulp.task('docs:api', function(cb) {
-  exec('ember ember-cli-yuidoc', function(err) {
-    cb(err);
-  });
+gulp.task('docs:api', function() {
+  return execa('ember', ['ember-cli-yuidoc']);
 });
 
-gulp.task('docs:app', function(cb) {
-  exec('ember build --prod', function(err) {
-    cb(err);
-  });
+gulp.task('docs:app', function() {
+  return execa('ember', ['build', '--prod']);
 });
 
 gulp.task('docs:publish', gulp.series(gulp.parallel('docs:api', 'docs:app'), function() {
@@ -29,11 +25,18 @@ gulp.task('docs:publish', gulp.series(gulp.parallel('docs:api', 'docs:app'), fun
 }));
 
 gulp.task('changelog', function() {
-  return gulp.src('CHANGELOG.md')
-    .pipe(conventionalChangelog({
-      preset: 'angular'
-    }))
-    .pipe(gulp.dest('./'));
+  let index = process.argv.indexOf('--tag');
+  let args = ['-s'];
+  if (index !== -1) {
+    let tag = process.argv[index + 1];
+    args.push('--next-version', tag);
+  }
+  return execa('lerna-changelog', args)
+    .then(({ stdout }) => {
+      return gulp.src('CHANGELOG.md')
+        .pipe(insert.prepend(stdout))
+        .pipe(gulp.dest('./'));
+    });
 });
 
 gulp.task('docs', gulp.series('docs:publish'));
