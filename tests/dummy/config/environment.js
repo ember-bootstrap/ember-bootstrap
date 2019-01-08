a322869ecefb083e8b36070f93f7a61b115eb4a3
Jeldrik Hanschke <admin@jhanschke.de>
2019-01-08 20:58:24 +0100

    whitelist frame-src https://ghbtns.com/ used in application template of dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -18,6 +18,16 @@ module.exports = function(environment) {
       // when it is created
     },
 
+    contentSecurityPolicy: {
+      'default-src': ["'none'"],
+      'script-src':  ["'self'"],
+      'font-src':    ["'self'"],
+      'connect-src': ["'self'"],
+      'img-src':     ["'self'"],
+      'style-src':   ["'self'"],
+      'media-src':   ["'self'"],
+      'frame-src':   ["https://ghbtns.com/"],
+    },
     contentSecurityPolicyMeta: true,
 
     fastboot: {
