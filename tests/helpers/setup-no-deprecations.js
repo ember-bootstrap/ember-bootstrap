3a5bb5830ac55a0583cd6265afda80e617c51fd5
simonihmig <simon.ihmig@kaliber5.de>
2019-02-17 19:18:47 +0100

    Fail only on for deprecations on Ember release, beta and canary

--- tests/helpers/setup-no-deprecations.js
+++ tests/helpers/setup-no-deprecations.js
@@ -1,25 +1,28 @@
 import { registerDeprecationHandler } from '@ember/debug';
+import config from 'dummy/config/environment';
 
 let isRegistered = false;
 let deprecations;
 
 export default function setupNoDeprecations({ beforeEach, afterEach }) {
 
-  beforeEach(function() {
-    deprecations = [];
-    if (!isRegistered) {
-      registerDeprecationHandler((message, options, next) => {
-        deprecations.push(message);
-        next(message, options);
-      });
-      isRegistered = true;
-    }
-  });
+  if (config.failOnDeprecation) {
+    beforeEach(function() {
+      deprecations = [];
+      if (!isRegistered) {
+        registerDeprecationHandler((message, options, next) => {
+          deprecations.push(message);
+          next(message, options);
+        });
+        isRegistered = true;
+      }
+    });
 
-  afterEach(function(assert) {
-    // guard in if instead of using assert.equal(), to not make assert.expect() fail
-    if (deprecations && deprecations.length > 0) {
-      assert.ok(false, `Expected no deprecations, found: ${deprecations.map(msg => `"${msg}"`).join(', ')}`);
-    }
-  });
+    afterEach(function(assert) {
+      // guard in if instead of using assert.equal(), to not make assert.expect() fail
+      if (deprecations && deprecations.length > 0) {
+        assert.ok(false, `Expected no deprecations, found: ${deprecations.map(msg => `"${msg}"`).join(', ')}`);
+      }
+    });
+  }
 }
