4e832695b00aaffbae8184c9253f5e3c34f11637
Jeldrik Hanschke <admin@jhanschke.de>
2019-02-22 08:33:37 +0100

    tests: remove work-a-round for qunit test runner CSP violation
    This is not needed anymore after CSP violations have been fixed upstream
    by https://github.com/qunitjs/qunit/pull/1369.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -8,18 +8,7 @@ if (typeof Promise === 'undefined') {
   window.Promise = EmberPromise;
 }
 
-document.addEventListener('securitypolicyviolation', function({ blockedURI, target, violatedDirective }) {
-  // ignore CSP violation by qunit
-  // This couldn't be whitelisted using SHA hash cause it's style-src-attribute violation.
-  // Could be removed after upstream fix is merged and released upstream:
-  // https://github.com/qunitjs/qunit/pull/1369
-  if (
-    violatedDirective.startsWith('style-src') &&
-    target.parentNode.id === 'qunit-modulefilter-actions'
-  ) {
-    return;
-  }
-
+document.addEventListener('securitypolicyviolation', function({ blockedURI, violatedDirective }) {
   throw new Error(
     'Content-Security-Policy violation detected: ' +
     `Violated directive: ${violatedDirective}. ` +
