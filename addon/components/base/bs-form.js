1ca67593779113db9dcb2a17a660ea3a6a27b180
Simon Ihmig <ihmig@kaliber5.de>
2019-02-27 20:15:16 +0100

    Update addon/components/base/bs-form.js
    Co-Authored-By: jelhan <jelhan@users.noreply.github.com>

--- addon/components/base/bs-form.js
+++ addon/components/base/bs-form.js
@@ -85,7 +85,7 @@ import layout from 'ember-bootstrap/templates/components/bs-form';
   ```
 
   Additionaly `isSubmitted` and `isRejected` properties are yielded. `isSubmitted` is `true` if last submission was successful.
-  `isRejected` is `true` if last submission was rejected due to validation errors or by action bound to `onSubmit` event.
+  `isRejected` is `true` if last submission was rejected due to validation errors or by an action bound to `onSubmit` event, returning a rejected promise.
   Both are reset as soon as any value of a form element changes. It could be used for visual feedback about last submission:
 
   ```
