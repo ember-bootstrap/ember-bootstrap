6fb73d1fc8bca494fc36a5cfb44bed591829e66b
simonihmig <simon.ihmig@kaliber5.de>
2019-03-24 02:05:31 +0100

    Deprecate nav.link-to

--- addon/components/base/bs-nav/link-to.js
+++ addon/components/base/bs-nav/link-to.js
@@ -1,5 +1,6 @@
 import LinkComponent from '@ember/routing/link-component';
 import ComponentChild from 'ember-bootstrap/mixins/component-child';
+import { deprecate } from '@ember/application/deprecations';
 
 /**
 
@@ -12,4 +13,15 @@ import ComponentChild from 'ember-bootstrap/mixins/component-child';
  @public
  */
 export default LinkComponent.extend(ComponentChild, {
+  init() {
+    this._super(...arguments);
+    deprecate('The link-to component yielded by bs-nav is deprecated. Use the linkTo property of the yielded item component instead!\n' +
+      'Before: {{#nav.item}}{{#nav.link-to "index"}}foo{{/nav.link-to}}{{/nav.item}}\n' +
+      'After: {{#nav.item linkTo="index"}}foo{{/nav.item}}',
+      false,
+      {
+        id: 'ember-bootstrap.nav.link-to',
+        until: '3.0'
+      });
+  }
 });
