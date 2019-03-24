ded86487c80c06a61d303fd55f4a11bf6ff44914
simonihmig <simon.ihmig@kaliber5.de>
2019-03-24 23:45:33 +0100

    Fix deprecations tests

--- tests/helpers/setup-no-deprecations.js
+++ tests/helpers/setup-no-deprecations.js
@@ -7,28 +7,30 @@ let deprecations;
 
 export default function setupNoDeprecations({ beforeEach, afterEach }) {
 
-  if (config.failOnDeprecation) {
-    beforeEach(function() {
-      deprecations = [];
-      if (!isRegistered) {
-        registerDeprecationHandler((message, options, next) => {
-          deprecations.push(message);
-          next(message, options);
-        });
-        isRegistered = true;
-      }
-    });
+  beforeEach(function() {
+    deprecations = [];
+    if (!isRegistered) {
+      registerDeprecationHandler((message, options, next) => {
+        deprecations.push(message);
+        next(message, options);
+      });
+      isRegistered = true;
+    }
+  });
 
-    afterEach(function(assert) {
-      // guard in if instead of using assert.equal(), to not make assert.expect() fail
-      if (deprecations && deprecations.length > 0) {
-        assert.ok(false, `Expected no deprecations, found: ${deprecations.map(msg => `"${msg}"`).join(', ')}`);
-      }
-    });
-  }
+  afterEach(function(assert) {
+    // guard in if instead of using assert.equal(), to not make assert.expect() fail
+    if (config.failOnDeprecation && deprecations && deprecations.length > 0) {
+      assert.ok(false, `Expected no deprecations, found: ${deprecations.map(msg => `"${msg}"`).join(', ')}`);
+    }
+  });
 
-  QUnit.assert.deprecations = function(count = 1) {
-    this.equal(deprecations.length, count, 'Expected deprecations during test.');
+  QUnit.assert.deprecations = function(count) {
+    if (count === undefined) {
+      this.ok(deprecations.length, 'Expected deprecations during test.');
+    } else {
+      this.equal(deprecations.length, count, `Expected ${count} deprecation(s) during test.`);
+    }
 
     deprecations = [];
   };
