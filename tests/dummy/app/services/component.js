f05750bceb92d6c1b7d0ba93e7e44f452fe41aa2
simonihmig <simon.ihmig@kaliber5.de>
2019-09-08 23:44:50 +0200

    Fix and improve API and Bootstrap docs links

--- tests/dummy/app/services/component.js
+++ tests/dummy/app/services/component.js
@@ -7,93 +7,93 @@ const componentData = A([
     id: 'accordion',
     title: 'Accordion',
     description: 'Toggle the display of content sections.',
-    bsUrl: 'http://getbootstrap.com/javascript/#collapse-example-accordion'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/collapse/#accordion-example'
   },
   {
     id: 'alert',
     title: 'Alert',
     description: 'Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.',
-    bsUrl: 'http://getbootstrap.com/components/#alerts'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/alerts/'
   },
   {
     id: 'button',
     title: 'Button',
     description: 'Add styling and advanced functionality to your buttons.',
-    bsUrl: 'http://getbootstrap.com/css/#buttons'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/buttons/'
   },
   {
     id: 'button-group',
     title: 'Button Group',
     className: 'ButtonGroup',
     description: 'Group a series of buttons together on a single line with the button group, with radio and checkbox style behavior.',
-    bsUrl: 'http://getbootstrap.com/components/#btn-groups'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/button-group/'
   },
   {
     id: 'carousel',
     title: 'Carousel',
     description: 'A slideshow component for cycling through elements.',
-    bsUrl: 'http://getbootstrap.com/javascript/#carousel'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/carousel/'
   },
   {
     id: 'collapse',
     title: 'Collapse',
     description: 'Easily hide and reveal additional content.',
-    bsUrl: 'http://getbootstrap.com/javascript/#collapse'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/collapse/'
   },
   {
     id: 'dropdown',
     title: 'Dropdown',
     description: 'Toggleable, contextual menu for displaying lists of links.',
-    bsUrl: 'http://getbootstrap.com/components/#dropdowns'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/dropdowns/'
   },
   {
     id: 'forms',
     title: 'Form',
     description: 'Advanced forms with support for easy markup generation and validation.',
-    bsUrl: 'http://getbootstrap.com/css/#forms'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/forms/'
   },
   {
     id: 'modal',
     title: 'Modal',
     description: 'Modals are streamlined, but flexible, dialog prompts with the minimum required functionality and smart defaults.',
-    bsUrl: 'http://getbootstrap.com/javascript/#modals'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/modal/'
   },
   {
     id: 'navbars',
     title: 'Navbar',
     description: 'Navbars are responsive meta components that serve as navigation headers for your application or site. They begin collapsed (and are toggleable) in mobile views and become horizontal as the available viewport width increases.',
-    bsUrl: 'http://getbootstrap.com/components/#navbar'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/navbar/'
   },
   {
     id: 'navs',
     title: 'Nav',
     description: 'Navigation components with default pills and tabs styles.',
-    bsUrl: 'http://getbootstrap.com/components/#nav'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/navs/'
   },
   {
     id: 'popover',
     title: 'Popover',
     description: 'Add small overlays of content, like those on the iPad, to any element for housing secondary information.',
-    bsUrl: 'http://getbootstrap.com/javascript/#popovers'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/popovers/'
   },
   {
     id: 'progress',
     title: 'Progress',
     description: 'Provide up-to-date feedback on the progress of a workflow or action with simple yet flexible progress bars.',
-    bsUrl: 'http://getbootstrap.com/components/#progress'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/progress/'
   },
   {
     id: 'tabs',
     title: 'Tabs',
     className: 'Tab',
     description: 'Add quick, dynamic tab functionality to transition through panes of local content, even via dropdown menus.',
-    bsUrl: 'http://getbootstrap.com/javascript/#tabs'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/navs/#tabs'
   },
   {
     id: 'tooltip',
     title: 'Tooltip',
     description: 'Add contextual information to any element.',
-    bsUrl: 'http://getbootstrap.com/javascript/#tooltips'
+    bsUrl: 'https://getbootstrap.com/docs/4.3/components/tooltips/'
   }
 ]);
 
