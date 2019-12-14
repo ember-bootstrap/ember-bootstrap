1aa44f88e3ea268bf3383b045634858b3753b0b4
simonihmig <simon.ihmig@kaliber5.de>
2019-12-14 01:55:25 +0100

    Update to Ember 3.14 blueprint

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('demo', { path: '/components' }, function() {
@@ -40,5 +40,3 @@ Router.map(function() {
     this.route('linkto', { path: '/linkto/:model' });
   });
 });
-
-export default Router;
