28b59a83bc75b2a1c2fc89b464ce5dff40ff6f25
Simon Ihmig <simon.ihmig@kaliber5.de>
2020-02-08 12:29:54 +0100

    Add additional has-bootstrap-version tests
    Co-Authored-By: Jeldrik Hanschke <jelhan@users.noreply.github.com>

--- tests/integration/helpers/has-bootstrap-version-test.js
+++ tests/integration/helpers/has-bootstrap-version-test.js
@@ -9,13 +9,17 @@ module('Integration | Helper | hasBootstrapVersion', function(hooks) {
 
   testBS3('it works', async function(assert) {
     await render(hbs`{{#if (has-bootstrap-version 3)}}Ok{{/if}}`);
-
     assert.dom(this.element).hasText('Ok');
+
+    await render(hbs`{{#if (has-bootstrap-version 4)}}Ok{{/if}}`);
+    assert.dom(this.element).hasNoText();
   });
 
   testBS4('it works', async function(assert) {
     await render(hbs`{{#if (has-bootstrap-version 4)}}Ok{{/if}}`);
-
     assert.dom(this.element).hasText('Ok');
+
+    await render(hbs`{{#if (has-bootstrap-version 3)}}Ok{{/if}}`);
+    assert.dom(this.element).hasNoText();
   });
 });
