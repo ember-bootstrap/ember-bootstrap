4bd90ea2a32f53f3583ef71dc65f528d153334b9
simonihmig <simon.ihmig@kaliber5.de>
2020-02-16 15:19:58 +0100

    Convert Form to tag-less / outerHTML component

--- addon/components/bs4/bs-form.js
+++ addon/components/bs4/bs-form.js
@@ -1,10 +1,1 @@
-import { computed } from '@ember/object';
-import BaseForm from 'ember-bootstrap/components/base/bs-form';
-
-export default class Form extends BaseForm {
-  @(computed('formLayout').readOnly())
-  get layoutClass() {
-    let layout = this.get('formLayout');
-    return layout === 'inline' ? 'form-inline' : null;
-  }
-}
+export { default } from 'ember-bootstrap/components/base/bs-form';
