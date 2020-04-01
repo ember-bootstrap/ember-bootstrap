a5da41022d3173a8fc3dcf80c6b47e224924d234
simonihmig <simon.ihmig@kaliber5.de>
2020-04-01 10:35:41 +0200

    Cleanup has-bootstrap helpers

--- addon/compatibility-helpers.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-undef
-const hasVersion = BOOTSTRAP_VERSION; // will be replaced at build time
-
-export function hasBootstrapVersion(requiredVersion) {
-  return requiredVersion === hasVersion;
-}
