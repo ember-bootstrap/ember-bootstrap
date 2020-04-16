2e45969a3aeaf86919bbb1ea0d3a2fb445be687f
Jeldrik Hanschke <admin@jhanschke.de>
2020-04-16 20:22:52 +0200

    remove custom solution to fail tests on CSP violations
    This features is provided by ember-cli-content-security-policy@v2 out of
    the box: https://github.com/rwjblue/ember-cli-content-security-policy#test-support

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -8,17 +8,5 @@ if (typeof Promise === 'undefined') {
   window.Promise = EmberPromise;
 }
 
-document.addEventListener('securitypolicyviolation', function({ target, blockedURI, violatedDirective }) {
-  if (target.className && target.className.match(/ember-basic-dropdown/) || target.hasAttribute && target.hasAttribute('data-test-ignore-csp')) {
-    return;
-  }
-
-  throw new Error(
-    'Content-Security-Policy violation detected: ' +
-    `Violated directive: ${violatedDirective}. ` +
-    `Blocked URI: ${blockedURI}`
-  );
-});
-
 setApplication(Application.create(config.APP));
 start();
