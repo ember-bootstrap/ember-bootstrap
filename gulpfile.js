ff8ffd4cb9bd1315cf97f7535aefbbea348d7ece
Jeldrik Hanschke <admin@jhanschke.de>
2020-04-16 22:16:01 +0200

    remove gulp task for generating changelog

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const execa = require('execa');
 const merge = require('merge-stream');
 const striptags = require('striptags');
 const transform = require('gulp-transform');
-const insert = require('gulp-insert');
 
 gulp.task('docs:api', function() {
   return execa('ember', ['ember-cli-yuidoc']);
@@ -25,20 +24,5 @@ gulp.task('docs:publish', gulp.series(gulp.parallel('docs:api', 'docs:app'), fun
   )
     .pipe(ghPages());
 }));
-
-gulp.task('changelog', function() {
-  let index = process.argv.indexOf('--tag');
-  let args = ['-s'];
-  if (index !== -1) {
-    let tag = process.argv[index + 1];
-    args.push('--next-version', tag);
-  }
-  return execa('node_modules/lerna-changelog/bin/cli.js', args)
-    .then(({ stdout }) => {
-      return gulp.src('CHANGELOG.md')
-        .pipe(insert.prepend(stdout))
-        .pipe(gulp.dest('./'));
-    });
-});
 
 gulp.task('docs', gulp.series('docs:publish'));
