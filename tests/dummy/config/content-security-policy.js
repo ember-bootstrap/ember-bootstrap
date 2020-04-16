6b7936d99fda844ebd9bc8f4f5b515a5df92c587
Jeldrik Hanschke <admin@jhanschke.de>
2020-04-16 20:19:51 +0200

    migrate configuration to config/content-security-policy.
    Configuration in config/environment.js is deprecated in
    ember-cli-content-security-policy@v2. See
    https://github.com/rwjblue/ember-cli-content-security-policy/blob/master/DEPRECATIONS.md#configuration-is-moved-to-configcontent-security-policyjs
    for details.

---
+++ tests/dummy/config/content-security-policy.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+  return {
+    delivery: ['meta'],
+    policy: {
+      'default-src':  ["'none'"],
+      'script-src':   ["'self'"],
+      'font-src':     ["'self'"],
+      'connect-src':  ["'self'"],
+      'img-src':      [
+        "'self'",
+        // Bootstrap 4 uses data URL for some SVG images in CSS
+        "data:",
+      ],
+      'style-src':    ["'self'"],
+      'media-src':    ["'self'"],
+    },
+  };
+}
