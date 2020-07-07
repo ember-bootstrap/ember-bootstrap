248983285b78d7d0fded12be2eeaf09a2f7cb991
simonihmig <simon.ihmig@kaliber5.de>
2020-07-07 20:51:31 +0200

    Fix subclassing deprecation, causing exceptions in BsModalSimple and BsNav
    Fixes #1132

--- addon/utils/deprecate-subclassing.js
+++ addon/utils/deprecate-subclassing.js
@@ -3,22 +3,22 @@ import { deprecate } from '@ember/debug';
 
 export default function deprecateSubclassing(target) {
   if (DEBUG) {
-    const originalInit = target.prototype.init;
+    const wrapperClass = class extends target {
+      init() {
+        deprecate(
+          `Extending from ember-bootstrap component classes is not supported, and might break anytime. Detected subclassing of <Bs${target.name}> component.`,
+          // the `__ember-bootstrap_subclass` flag is an escape hatch for "privileged" addons like validations addons that currently still have to rely on subclassing
+          wrapperClass === this.constructor || this['__ember-bootstrap_subclass'] === true,
+          {
+            id: `ember-bootstrap.subclassing#${target.name}`,
+            until: '5.0.0',
+          }
+        );
 
-    target.prototype.init = function () {
-      deprecate(
-        `Extending from ember-bootstrap component classes is not supported, and might break anytime. Detected subclassing of <Bs${target.name}> component.`,
-        // the `__ember-bootstrap_subclass` flag is an escape hatch for "privileged" addons like validations addons that currently still have to rely on subclassing
-        target === this.constructor || this['__ember-bootstrap_subclass'] === true,
-        {
-          id: `ember-bootstrap.subclassing#${target.name}`,
-          until: '5.0.0',
-        }
-      );
-
-      if (originalInit) {
-        return originalInit.apply(this, arguments);
+        return super.init(...arguments);
       }
     };
+
+    return wrapperClass;
   }
 }
