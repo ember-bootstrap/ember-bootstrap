34379a45c0e7e9db69a41913bf78b7f4973bece9
simonihmig <simon.ihmig@kaliber5.de>
2021-05-13 15:12:15 +0200

    Add BS5 scenarios to CI

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const bootstrapVersion = process.env.BOOTSTRAPVERSION === '3' ? '^3.4.1' : '^4.3.1';
+const bootstrapVersion = `^${process.env.BOOTSTRAPVERSION}.0.0`;
 const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
