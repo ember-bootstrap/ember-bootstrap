bcdf7bacca2e156b8200fe8b1dee946c76f78e0b
simonihmig <simon.ihmig@kaliber5.de>
2021-08-11 22:31:45 +0200

    Revert IE11 workaround of #775

--- addon/components/bs-form/element/control/input.js
+++ addon/components/bs-form/element/control/input.js
@@ -6,27 +6,6 @@ import sizeClass from 'ember-bootstrap/utils/cp/size-class';
 import defaultValue from 'ember-bootstrap/utils/default-decorator';
 import deprecateSubclassing from 'ember-bootstrap/utils/deprecate-subclassing';
 
-const allowedTypes = new Map();
-function canUseType(type) {
-  if (typeof document !== 'object' || typeof document.createElement !== 'function') {
-    // consider all types as supported if running in an
-    // environment that doesn't support DOM
-    return true;
-  }
-
-  if (!allowedTypes.has(type)) {
-    try {
-      let inputElement = document.createElement('input');
-      inputElement.type = type;
-      allowedTypes.set(type, true);
-    } catch (error) {
-      allowedTypes.set(type, false);
-    }
-  }
-
-  return allowedTypes.get(type);
-}
-
 /**
 
  @class FormElementControlInput
@@ -42,27 +21,7 @@ export default class FormElementControlInput extends Control {
    * @type {String}
    * @public
    */
-  @computed
-  get type() {
-    return 'text';
-  }
-  set type(value) {
-    // fallback to 'text' if value is empty
-    if (isEmpty(value)) {
-      return 'text';
-    }
-
-    // IE 11 throws if setting an unsupported type via DOM.
-    // We guard against that behaviour by testing if user
-    // agent throws on setting the provided type.
-    // This is inspired by input helper shipped with Ember.js:
-    // https://github.com/emberjs/ember.js/blob/30137796af42c63b28ead127cba0e43e45a773c1/packages/%40ember/-internals/glimmer/lib/components/text_field.ts#L93-L115
-    if (!canUseType(value)) {
-      return 'text';
-    }
-
-    return value;
-  }
+  type = 'text';
 
   @action
   handleChange(event) {
--- tests/integration/components/bs-form/element/control/input-test.js
+++ tests/integration/components/bs-form/element/control/input-test.js
@@ -21,16 +21,6 @@ module('Integration | Component | bs form/element/control/input', function (hook
     assert.dom('input[type=text]').hasValue('FOO');
   });
 
-  test('it falls back to text if type is not supported by browser', async function (assert) {
-    // This also asserts that IE 11 issue of throwing if setting an unsupported type
-    // is set via DOM (e.g. document.createElement('input').type = "foo") is handled
-    // well.
-    await render(hbs`<BsForm::Element::Control::Input @type="foo" />`);
-
-    // qunit-dom does not provide a method to assert that a property equals a value
-    assert.equal(find('input').type, 'text');
-  });
-
   testNotBS3('support size classes', async function (assert) {
     await render(hbs`<BsForm::Element::Control::Input @size="lg" />`);
     assert.dom('input').hasClass('form-control-lg', 'input has large class');
