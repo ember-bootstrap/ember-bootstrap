9f4d76fa5c37fa146d112150833292a6c306eca6
simonihmig <simon.ihmig@kaliber5.de>
2021-08-18 00:26:59 +0200

    Fix test

--- tests/fastboot/accordion-test.js
+++ tests/fastboot/accordion-test.js
@@ -1,6 +1,6 @@
 import { module } from 'qunit';
 import { setup, visit } from 'ember-cli-fastboot-testing/test-support';
-import { test } from '../helpers/bootstrap';
+import { accordionClass, accordionItemClass, accordionItemHeadClass, test } from '../helpers/bootstrap';
 
 module('FastBoot | accordion', function (hooks) {
   setup(hooks);
@@ -8,8 +8,8 @@ module('FastBoot | accordion', function (hooks) {
   test('it renders', async function (assert) {
     await visit('/fastboot/accordion');
 
-    assert.dom('.accordion').exists();
-    assert.dom('.accordion .card').exists({ count: 3 });
-    assert.dom('.accordion .card .card-header').hasText('First item');
+    assert.dom(`.${accordionClass()}`).exists();
+    assert.dom(`.${accordionClass()} .${accordionItemClass()}`).exists({ count: 3 });
+    assert.dom(`.${accordionClass()} .${accordionItemClass()} .${accordionItemHeadClass()}`).hasText('First item');
   });
 });
