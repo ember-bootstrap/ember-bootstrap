a17f91bad44bdcbd8527930fd31b9512e590b2a5
simonihmig <simon.ihmig@kaliber5.de>
2021-09-30 18:11:14 +0200

    Update modal page

--- docs/app/controllers/demo/modal.js
+++ docs/app/controllers/demo/modal.js
@@ -1,59 +1,60 @@
-import { oneWay } from '@ember/object/computed';
+import { action } from '@ember/object';
 import Controller from '@ember/controller';
-import { A } from '@ember/array';
 import { next } from '@ember/runloop';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  modal1: false,
-  modal2: true,
-  modal3: false,
-  hasModal3: false,
+export default class ModalController extends Controller {
+  @tracked
+  modal1 = false;
+  @tracked
+  modal2 = true;
+  @tracked
+  modal3 = false;
+  @tracked
+  hasModal3 = false;
+  @tracked
+  title = 'Simple Modal';
+  @tracked
+  sizes = ['', 'sm', 'lg'];
+  @tracked
+  size = '';
+  @tracked
+  closeButton = true;
+  @tracked
+  closeTitle = 'Ok';
+  @tracked
+  submitTitle = null;
+  @tracked
+  backdrop = true;
+  @tracked
+  backdropClose = true;
+  @tracked
+  fade = true;
 
-  title: 'Simple Modal',
-  sizes: A(['', 'sm', 'lg']),
-  size: oneWay('sizes.firstObject'),
-  closeButton: true,
-  closeTitle: 'Ok',
-  submitTitle: null,
-  backdrop: true,
-  backdropClose: true,
-  fade: true,
+  _ignoreClose = false;
 
-  _ignoreClose: false,
-
-  actions: {
-    addModal3() {
-      this.set('hasModal3', true);
-      this.set('modal3', true);
-    },
-    removeModal3() {
-      this.set('hasModal3', false);
-    },
-    submit() {
-      alert('Modal submitted!');
-    },
-    close() {
-      if (!this._ignoreClose) {
-        this.set('modal2', false);
-        alert('Modal closed!');
-      }
-    },
-    open() {
-      this.set('modal2', true);
-    },
-    backdropChange(backdrop) {
-      this._ignoreClose = true;
-      let fade = this.fade;
-      this.set('fade', false);
-      this.set('modal2', false);
-      next(() => {
-        this.set('backdrop', backdrop);
-        this.set('modal2', true);
-        next(() => {
-          this.set('fade', fade);
-          this._ignoreClose = false;
-        });
-      });
-    },
-  },
-});
+  @action
+  addModal3() {
+    this.hasModal3 = true;
+    this.modal3 = true;
+  }
+  @action
+  removeModal3() {
+    this.hasModal3 = false;
+  }
+  @action
+  submit() {
+    alert('Modal submitted!');
+  }
+  @action
+  close() {
+    if (!this._ignoreClose) {
+      this.modal2 = false;
+      alert('Modal closed!');
+    }
+  }
+  @action
+  open() {
+    this.modal2 = true;
+  }
+}
