379a707e250c9a6dbcd511935a37fb03685b4289
simonihmig <simon.ihmig@kaliber5.de>
2021-09-30 19:13:06 +0200

    Update progress page

--- docs/app/controllers/demo/progress.js
+++ docs/app/controllers/demo/progress.js
@@ -1,16 +1,13 @@
-import { not } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
-export default Controller.extend({
-  value: 50,
-  minValue: 0,
-  maxValue: 100,
-  showLabel: false,
-  type: 'default',
-  striped: false,
-  animate: false,
-  roundDigits: 0,
-  /* eslint-disable ember/avoid-leaking-state-in-ember-objects */
-  typeChoices: ['default', 'success', 'info', 'warning', 'danger'],
-  notStriped: not('striped'),
-});
+export default class ProgressController extends Controller {
+  value = 50;
+  minValue = 0;
+  maxValue = 100;
+  showLabel = false;
+  type = 'default';
+  striped = false;
+  animate = false;
+  roundDigits = 0;
+  typeChoices = ['default', 'success', 'info', 'warning', 'danger'];
+}
