703109d7bb18be313304ec622f5f4f97c7c541de
simonihmig <simon.ihmig@kaliber5.de>
2021-09-30 19:08:56 +0200

    Update navs page

--- docs/app/controllers/demo/navs.js
+++ docs/app/controllers/demo/navs.js
@@ -1,32 +1,32 @@
-import { oneWay } from '@ember/object/computed';
+import { action } from '@ember/object';
 import Controller from '@ember/controller';
-import { A } from '@ember/array';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  type: oneWay('typeChoices.firstObject'),
-  stacked: false,
-  justified: false,
-  fill: false,
-  typeChoices: A(['pills', 'tabs']),
+export default class NavsController extends Controller {
+  @tracked
+  type = 'pills';
+  @tracked
+  stacked = false;
+  @tracked
+  justified = false;
+  @tracked
+  fill = false;
 
-  actions: {
-    updateType(type) {
-      let props = {
-        type,
-      };
-      if (type === 'tabs') {
-        props.stacked = false;
-      }
-      this.setProperties(props);
-    },
-    updateStacked(stacked) {
-      let props = {
-        stacked,
-      };
-      if (stacked) {
-        props.justified = false;
-      }
-      this.setProperties(props);
-    },
-  },
-});
+  typeChoices = ['pills', 'tabs'];
+
+  @action
+  updateType(type) {
+    this.type = type;
+    if (type === 'tabs') {
+      this.stacked = false;
+    }
+  }
+
+  @action
+  updateStacked(stacked) {
+    this.stacked = stacked;
+    if (stacked) {
+      this.justified = false;
+    }
+  }
+}
