13fa68df63ea5318beaa45577227d30a218bdd42
simonihmig <simon.ihmig@kaliber5.de>
2022-01-08 17:05:14 +0100

    Accept loosely types values for modal `@open`
    Fixes #1612

--- addon/components/bs-modal.js
+++ addon/components/bs-modal.js
@@ -639,7 +639,7 @@ export default class Modal extends Component {
 
   @action
   handleVisibilityChanges() {
-    if (this.args.open !== false) {
+    if (this.open) {
       this.show();
     } else {
       this.hide();
