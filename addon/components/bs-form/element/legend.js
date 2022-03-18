84bdf1b00622c607467080edca221fbdf5589680
simonihmig <simon.ihmig@kaliber5.de>
2022-03-18 17:23:30 +0100

    Convert label to glimmer

--- addon/components/bs-form/element/legend.js
+++ addon/components/bs-form/element/legend.js
@@ -1,8 +1,6 @@
-import { tagName } from '@ember-decorators/component';
 import FormElementLabel from 'ember-bootstrap/components/bs-form/element/label';
 import deprecateSubclassing from 'ember-bootstrap/utils/deprecate-subclassing';
 
-@tagName('')
 @deprecateSubclassing
 export default class FormElementLegend extends FormElementLabel {
   '__ember-bootstrap_subclass' = true;
