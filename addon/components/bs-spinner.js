641619dce65812168d7ff47bcead2d2db57359a7
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2022-07-08 16:50:16 +0300

    Add Bootstrap Spinner Component

---
+++ addon/components/bs-spinner.js
@@ -0,0 +1,88 @@
+import Component from '@glimmer/component';
+import { getOwnConfig, macroCondition } from '@embroider/macros';
+import arg from '../utils/decorators/arg';
+
+/**
+ Implements a [Spinner element](https://getbootstrap.com/docs/5.2/components/spinners/)
+
+ ### Basic Usage
+
+ ```hbs
+ <BsSpinner/>
+ ```
+
+ ### Advanced Usage
+
+ ```hbs
+ <BsSpinner @type="glow" @size="sm" @type="warning"/>
+ ```
+
+ ### Usage with `BsButton`
+
+ ```hbs
+ <BsButton @onClick=(action "download") as |button|>
+  Download
+  {{#if button.isPending}}
+    <BsSpinner @size="sm"/>
+  {{/if}}
+ </BsButton>
+ ```
+
+ @class Spinner
+ @namespace Components
+ @extends Glimmer.Component
+ @public
+*/
+export default class SpinnerComponent extends Component {
+  /**
+   * Property for size styling, set to 'sm'
+   *
+   * Also see the [Bootstrap docs](https://getbootstrap.com/docs/5.2/components/spinners/#size)
+   *
+   * @property size
+   * @type String
+   * @public
+   */
+
+  /**
+   * Property for type styling
+   *
+   * For the available types see the [Bootstrap docs](https://getbootstrap.com/docs/5.2/components/spinners/#colors)
+   *
+   * @property type
+   * @type String
+   * @default 'primary'
+   * @public
+   */
+
+  /**
+   * Spinner type - 'border' or 'glow'
+   *
+   * @property spinnerType
+   * @type string
+   * @default 'border'
+   * @public
+   */
+  @arg
+  spinnerType = 'border';
+
+  /**
+   * @property accessibilityText
+   * @type string
+   * @public
+   */
+  @arg
+  accessibilityText = 'Loading...';
+
+  get spinnerClass() {
+    return `spinner-${this.spinnerType}`;
+  }
+
+  get accessibilityTextClass() {
+    if (macroCondition(getOwnConfig().isBS4)) {
+      return 'sr-only';
+    } else {
+      return 'visually-hidden';
+    }
+  }
+}
---
+++ tests/integration/components/bs-spinner-test.js
@@ -0,0 +1,70 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { testForBootstrap } from '../../helpers/bootstrap';
+
+module('Integration | Component | bs-spinner', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('spinner has correct default markup', async function (assert) {
+    await render(hbs`<BsSpinner/>`);
+
+    assert.dom('div').hasClass('spinner-border', 'spinner has spinner-border class');
+  });
+
+  test('spinner has correct size', async function (assert) {
+    await render(hbs`<BsSpinner @size="sm" />`);
+
+    assert.dom('div').hasClass('spinner-border-sm', 'spinner has size class');
+  });
+
+  test('spinner has correct type', async function (assert) {
+    await render(hbs`<BsSpinner @type="primary"/>`);
+
+    assert.dom('div').hasClass('text-primary', 'spinner has text-primary class');
+  });
+
+  testForBootstrap(4, 'spinner has accessibility block', async function (assert) {
+    await render(hbs`<BsSpinner/>`);
+
+    assert.dom('span').hasClass('rs-only', 'accessibility text exists');
+  });
+
+  testForBootstrap(5, 'spinner has accessibility block', async function (assert) {
+    await render(hbs`<BsSpinner/>`);
+
+    assert.dom('span').hasClass('visually-hidden', 'accessibility text exists');
+  });
+
+  test('spinner has default accessibility text', async function (assert) {
+    await render(hbs`<BsSpinner/>`);
+
+    assert.dom('span').hasText('Loading...');
+  });
+
+  test('spinner has HTML attributes', async function (assert) {
+    await render(hbs`<BsSpinner id="test"/>`);
+
+    assert.equal(this.element.querySelector('div').getAttribute('id'), 'test');
+    assert.equal(this.element.querySelector('div').getAttribute('role'), 'status');
+  });
+
+  test('spinnerType property allows changing spinner type', async function (assert) {
+    await render(hbs`<BsSpinner @spinnerType="glow" />`);
+
+    assert.dom('div').hasClass('spinner-glow');
+  });
+
+  test('accessibilityText property allows changing accessibility text', async function (assert) {
+    await render(hbs`<BsSpinner @accessibilityText="Custom text" />`);
+
+    assert.dom('span').hasText('Custom text', 'Custom text is shown');
+  });
+
+  test('type property allows changing spinner color', async function (assert) {
+    await render(hbs`<BsSpinner @type="warning" />`);
+
+    assert.dom('div').hasClass('text-warning', 'Spinner has warning-color');
+  });
+});
