0b947538b6fad297b9ba3f4d275e1a2244babdfc
Jeldrik Hanschke <jelhan@users.noreply.github.com>
2023-01-18 22:32:02 +0100

    clean API docs for removed property (BS3 only) (#1825)

--- addon/components/bs-button-group.js
+++ addon/components/bs-button-group.js
@@ -79,25 +79,6 @@ export default class ButtonGroup extends Component {
    */
 
   /**
-   * Set to true for the buttons to stretch at equal sizes to span the entire width of its parent. (BS3 only!)
-   *
-   * *Important*: You have to wrap every button component in a `div class="btn-group">`:
-   *
-   * ```handlebars
-   * <div class="btn-group" role="group">
-   * {{#bs-button}}My Button{{/bs-button}}
-   * </div>
-   * ```
-   *
-   * See http://getbootstrap.com/components/#btn-groups-justified
-   *
-   * @property justified
-   * @type boolean
-   * @default false
-   * @public
-   */
-
-  /**
    * The type of the button group specifies how child buttons behave and how the `value` property will be computed:
    *
    * ### null
