1e5fcd9608a2d564ebea8e609d7c496d5374838c
Jeldrik Hanschke <admin@jhanschke.de>
2023-03-01 23:01:41 +0100

    improve carousel test

--- docs/tests/acceptance/components/carousel-test.js
+++ docs/tests/acceptance/components/carousel-test.js
@@ -1,18 +1,46 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, fillIn, waitFor } from '@ember/test-helpers';
+import {
+  visit,
+  currentURL,
+  fillIn,
+  find,
+  settled,
+  waitFor,
+} from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Acceptance | components/carousel', function (hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /components/carousel', async function (assert) {
+    // Control the time to speed up test
+    const clock = sinon.useFakeTimers();
+
     // Ember never enters settled state after entering the route due to auto play
     visit('/components/carousel');
 
-    // Wait until Ember rendered the UI and stop playing
+    // Wait until Ember rendered the UI
     await waitFor('[data-test-option="interval"]');
-    await fillIn('[data-test-option="interval"] input', 0);
 
     assert.equal(currentURL(), '/components/carousel');
+    assert
+      .dom('[data-test-example="main"] .carousel-item.active img')
+      .hasAttribute('alt', 'First slide', 'shows the first slide');
+
+    const intervalBetweenSlides = find(
+      '[data-test-option="interval"] input'
+    ).value;
+
+    // Stop carousel from playing to enter settled state
+    fillIn('[data-test-option="interval"] input', 0);
+
+    // move time forward to have next slide shown immediately
+    clock.tick(intervalBetweenSlides * 1000);
+    await settled();
+
+    assert
+      .dom('[data-test-example="main"] .carousel-item.active img')
+      .hasAttribute('alt', 'Second slide', 'shows the second slide');
   });
 });
