579a14f34a20f005f9aa9d37ba4aa70823f7dfed
Jeldrik Hanschke <admin@jhanschke.de>
2023-03-01 21:36:54 +0100

    enable carousel test

--- docs/tests/acceptance/components/carousel-test.js
+++ docs/tests/acceptance/components/carousel-test.js
@@ -1,12 +1,17 @@
-import { module, skip } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { visit, currentURL, fillIn, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | components/carousel', function (hooks) {
   setupApplicationTest(hooks);
 
-  skip('visiting /components/carousel', async function (assert) {
-    await visit('/components/carousel');
+  test('visiting /components/carousel', async function (assert) {
+    // Ember never enters settled state after entering the route due to auto play
+    visit('/components/carousel');
+
+    // Wait until Ember rendered the UI and stop playing
+    await waitFor('[data-test-option="interval"]');
+    await fillIn('[data-test-option="interval"] input', 0);
 
     assert.equal(currentURL(), '/components/carousel');
   });
