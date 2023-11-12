247ed53afa552847307951de11190add4d31e69c
Jeldrik Hanschke <jelhan@users.noreply.github.com>
2023-11-12 12:41:28 +0100

    upgrade docs app with Ember CLI 4.12 blueprints (#2009)

--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -3,7 +3,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
+  const app = new EmberApp(defaults, {
     'ember-bootstrap': {
       bootstrapVersion: 5,
       importBootstrapCSS: false,
@@ -43,19 +43,6 @@ module.exports = function (defaults) {
       ],
     },
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
