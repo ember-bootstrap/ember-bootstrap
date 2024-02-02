97b0be80446c8247a2d57e6c45291f5a6fd2ed46
Sander Knauff <sanderknauff@hotmail.com>
2024-02-02 23:07:49 +0100

    [docs] Convert Changelog route to a native class and fixes rendering of changelog
    Between `ember-cli-showdown` 7.x and 8.x, the ability to use positional parameters was removed. It now uses angle-brackets syntax and named paramters

--- docs/app/routes/changelog.js
+++ docs/app/routes/changelog.js
@@ -1,9 +1,9 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
 
-export default Route.extend({
+export default class ChangelogRoute extends Route {
   async model() {
     let response = await fetch('/CHANGELOG.md');
     return response.text();
-  },
-});
+  }
+}
