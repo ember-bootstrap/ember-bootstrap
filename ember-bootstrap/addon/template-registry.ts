5cdf0ef8a9a996e84079f508d04c637fb7acb858
Jeldrik Hanschke <admin@jhanschke.de>
2024-02-28 21:49:53 +0100

    convert BsDropdown::Menu::Divider to TypeScript

--- ember-bootstrap/addon/template-registry.ts
+++ ember-bootstrap/addon/template-registry.ts
@@ -1,7 +1,10 @@
 import type BsButton from './components/bs-button';
 import type BsCollapse from './components/bs-collapse';
+import type BsDropdownMenuDivider from './components/bs-dropdown/menu/divider';
 
 export default interface EmberBootstrapRegistry {
   BsButton: typeof BsButton;
   BsCollapse: typeof BsCollapse;
+  'BsDropdown::Menu::Divider': typeof BsDropdownMenuDivider;
+  'bs-dropdown/menu/divider': typeof BsDropdownMenuDivider;
 }
