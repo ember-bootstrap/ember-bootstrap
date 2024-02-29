d329eeda06c84e4406959977cb35791ff3a0bfb3
Kirill Shaplyko <Baltazore@users.noreply.github.com>
2024-02-29 10:27:19 +0100

    Convert BsSpinner to TypeScript (#2108)
    Co-authored-by: Jeldrik Hanschke <jelhan@users.noreply.github.com>

--- ember-bootstrap/addon/template-registry.ts
+++ ember-bootstrap/addon/template-registry.ts
@@ -1,7 +1,9 @@
 import type BsButton from './components/bs-button';
 import type BsCollapse from './components/bs-collapse';
+import type BsSpinner from './components/bs-spinner';
 
 export default interface EmberBootstrapRegistry {
   BsButton: typeof BsButton;
   BsCollapse: typeof BsCollapse;
+  BsSpinner: typeof BsSpinner;
 }
