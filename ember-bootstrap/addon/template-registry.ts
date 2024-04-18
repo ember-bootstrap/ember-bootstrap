c94228fd1aa5cafdf1ba7be200ff0728cd4d386a
Jeldrik Hanschke <admin@jhanschke.de>
2024-04-18 22:23:48 +0200

    convert BsDropdown::Button to TypeScript

--- ember-bootstrap/addon/template-registry.ts
+++ ember-bootstrap/addon/template-registry.ts
@@ -1,5 +1,7 @@
 import type BsButton from './components/bs-button';
 import type BsCollapse from './components/bs-collapse';
+import type BsDropdownButton from './components/bs-dropdown/button';
+import type BsDropdownMenu from './components/bs-dropdown/menu';
 import type BsDropdownMenuDivider from './components/bs-dropdown/menu/divider';
 import type BsDropdownMenuItem from './components/bs-dropdown/menu/item';
 import type BsDropdownToggle from './components/bs-dropdown/toggle';
@@ -8,6 +10,10 @@ import type BsSpinner from './components/bs-spinner';
 export default interface EmberBootstrapRegistry {
   BsButton: typeof BsButton;
   BsCollapse: typeof BsCollapse;
+  'BsDropdown::Button': typeof BsDropdownButton;
+  'bs-dropdown/button': typeof BsDropdownButton;
+  'BsDropdown::Menu': typeof BsDropdownMenu;
+  'bs-dropdown/menu': typeof BsDropdownMenu;
   'BsDropdown::Menu::Divider': typeof BsDropdownMenuDivider;
   'bs-dropdown/menu/divider': typeof BsDropdownMenuDivider;
   'BsDropdown::Menu::Item': typeof BsDropdownMenuItem;
