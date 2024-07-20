e7207cf12c31ba6c7828b7d190dc62b93cc2a9d9
Sander Knauff <sanderknauff@hotmail.com>
2024-07-20 18:56:27 +0200

    Tests: Add support for writing tests in Gjs/Gts
    When changing the tests to get rid of the action helpers, it might be a good time to also update the tests to Gjs/Gts.
    The changes for the action helper will be influencing how we access scope variables in the tests, and so will using ember-template-import syntax.
    The commit prepares the test-app for template-imports and converts one file to gjs and one to gts as a proof-of-concept and to verify that the configuration of tools such as ESLint and Prettier still work correctly.

--- test-app/.template-lintrc.js
+++ test-app/.template-lintrc.js
@@ -8,23 +8,7 @@ module.exports = {
     'attribute-indentation': false,
     'no-pointer-down-event-binding': false,
   },
-  ignore: ['fastboot-tests/fixtures/**/*'],
   overrides: [
-    {
-      files: ['docs/**/*'],
-      rules: {
-        // @todo we need to fix these in the docs app!
-        'no-action': 'warn',
-        'no-autofocus-attribute': 'warn',
-        'no-curly-component-invocation': 'warn',
-        'no-implicit-this': 'warn',
-        'no-invalid-link-text': 'warn',
-        'no-link-to-positional-params': 'warn',
-        'no-valueless-arguments': 'warn',
-        'require-context-role': 'warn',
-        'require-presentational-children': 'warn',
-      },
-    },
     {
       files: ['tests/**/*'],
       rules: {
@@ -36,6 +20,12 @@ module.exports = {
         'no-invalid-role': 'warn',
         'require-input-label': 'warn',
         'require-presentational-children': 'warn',
+      },
+    },
+    {
+      files: ['tests/**/*.(gjs|gts)'],
+      rules: {
+        prettier: 'off',
       },
     },
   ],
