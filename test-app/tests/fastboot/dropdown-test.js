6af60ee60050b375a5f1a8c5248bd49e95e32304
Jeldrik Hanschke <admin@jhanschke.de>
2025-11-13 10:51:50 +0100

    skip Fastboot tests failing due to a regression in Ember >= 6.8

--- test-app/tests/fastboot/dropdown-test.js
+++ test-app/tests/fastboot/dropdown-test.js
@@ -1,14 +1,21 @@
-import { module, test } from 'qunit';
+import { module, skip, test } from 'qunit';
 import { setup, visit } from 'ember-cli-fastboot-testing/test-support';
+import { dependencySatisfies } from '@embroider/macros';
 
 module('FastBoot | dropdown', function (hooks) {
   setup(hooks);
 
-  test('it renders', async function (assert) {
-    await visit('/fastboot/dropdown');
+  // A regression in Ember.js >= 6.8.0 breaks Fastboot in some cases.
+  // We need to disable the affected tests for now.
+  // The issue is tracked here: https://github.com/emberjs/ember.js/issues/20984
+  (dependencySatisfies('ember-source', '<= 6.7.0') ? test : skip)(
+    'it renders',
+    async function (assert) {
+      await visit('/fastboot/dropdown');
 
-    assert.dom('.dropdown').exists();
-    assert.dom('.dropdown .dropdown-toggle').exists();
-    assert.dom('.dropdown .dropdown-toggle').hasText('Dropdown');
-  });
+      assert.dom('.dropdown').exists();
+      assert.dom('.dropdown .dropdown-toggle').exists();
+      assert.dom('.dropdown .dropdown-toggle').hasText('Dropdown');
+    },
+  );
 });
--- test-app/tests/fastboot/nav-test.js
+++ test-app/tests/fastboot/nav-test.js
@@ -1,14 +1,21 @@
-import { module, test } from 'qunit';
+import { module, skip, test } from 'qunit';
 import { setup, visit } from 'ember-cli-fastboot-testing/test-support';
+import { dependencySatisfies } from '@embroider/macros';
 
 module('FastBoot | nav', function (hooks) {
   setup(hooks);
 
-  test('it renders', async function (assert) {
-    await visit('/fastboot/navs');
+  // A regression in Ember.js >= 6.8.0 breaks Fastboot in some cases.
+  // We need to disable the affected tests for now.
+  // The issue is tracked here: https://github.com/emberjs/ember.js/issues/20984
+  (dependencySatisfies('ember-source', '<= 6.7.0') ? test : skip)(
+    'it renders',
+    async function (assert) {
+      await visit('/fastboot/navs');
 
-    assert.dom('.nav').exists();
-    assert.dom('.nav li').exists({ count: 3 });
-    assert.dom('.nav li a').exists({ count: 3 });
-  });
+      assert.dom('.nav').exists();
+      assert.dom('.nav li').exists({ count: 3 });
+      assert.dom('.nav li a').exists({ count: 3 });
+    },
+  );
 });
