c1858af8aeddfd8d8105f185b8a15bbbbb91f1fe
Jeldrik Hanschke <jelhan@users.noreply.github.com>
2026-01-14 08:02:43 +0100

    remove warnings for invalid bootstrap version config (#2255)

--- ember-bootstrap/index.js
+++ ember-bootstrap/index.js
@@ -2,15 +2,10 @@
 
 const path = require('path');
 const BroccoliDebug = require('broccoli-debug');
-const SilentError = require('silent-error'); // From ember-cli
-const VersionChecker = require('ember-cli-version-checker');
 
 const defaultOptions = {
   bootstrapVersion: 5,
 };
-
-const minimumBS4Version = '4.0.0-beta';
-const minimumBS5Version = '5.0.0';
 
 module.exports = {
   name: require('./package').name,
@@ -26,7 +21,6 @@ module.exports = {
     this._super.included.apply(this, arguments);
 
     let app = this._findHost(this);
-    this.app = app;
 
     let options = Object.assign(
       {},
@@ -34,18 +28,10 @@ module.exports = {
       app.options['ember-bootstrap'],
     );
 
-    if (options.bootstrapVersion === 3) {
-      throw new SilentError(
-        'Support for Bootstrap 3 has been removed as of ember-bootstrap v5! Consider migrating to Bootstrap v4 or v5, or downgrade ember-bootstrap to v4!',
-      );
-    }
-
-    this.bootstrapOptions = options;
-
-    this.validateDependencies();
+    const bootstrapVersion = parseInt(options.bootstrapVersion);
 
     // import custom addon CSS required to use Popper.js v2 with Bootstrap v4
-    if (options.bootstrapVersion === 4) {
+    if (bootstrapVersion === 4) {
       let vendorPath = path.join('vendor', 'ember-bootstrap');
 
       // import custom addon CSS
@@ -54,11 +40,11 @@ module.exports = {
 
     // setup config for @embroider/macros
     this.options['@embroider/macros'].setOwnConfig.isBS4 =
-      this.getBootstrapVersion() === 4;
+      bootstrapVersion === 4;
     this.options['@embroider/macros'].setOwnConfig.isBS5 =
-      this.getBootstrapVersion() === 5;
+      bootstrapVersion === 5;
     this.options['@embroider/macros'].setOwnConfig.isNotBS5 =
-      this.getBootstrapVersion() !== 5;
+      bootstrapVersion !== 5;
     this.options['@embroider/macros'].setOwnConfig.version =
       require('./package.json').version;
   },
@@ -68,30 +54,5 @@ module.exports = {
       setOwnConfig: {},
     },
     'ember-cli-babel': { enableTypeScriptTransform: true },
-  },
-
-  validateDependencies() {
-    let checker = new VersionChecker(this.project);
-    let dep = checker.for('bootstrap');
-
-    if (this.getBootstrapVersion() === 4 && !dep.gte(minimumBS4Version)) {
-      this.warn(
-        `For Bootstrap 4 support this version of ember-bootstrap requires at least Bootstrap ${minimumBS4Version}, but you have ${dep.version}. Please run \`ember generate ember-bootstrap\` to update your dependencies!`,
-      );
-    }
-
-    if (this.getBootstrapVersion() === 5 && !dep.gte(minimumBS5Version)) {
-      this.warn(
-        `For Bootstrap 5 support this version of ember-bootstrap requires at least Bootstrap ${minimumBS5Version}, but you have ${dep.version}. Please run \`ember generate ember-bootstrap\` to update your dependencies!`,
-      );
-    }
-  },
-
-  getBootstrapVersion() {
-    return parseInt(this.bootstrapOptions.bootstrapVersion);
-  },
-
-  warn(message) {
-    this.ui.writeWarnLine(message);
   },
 };
