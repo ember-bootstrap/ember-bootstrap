1e06d83be9780f7cc655e7c69fce9bf27356cc64
Sander Knauff <sanderknauff@hotmail.com>
2026-04-19 18:40:41 +0200

    updated typing of on-window helper from registry entry to exported function

--- ember-bootstrap/types/ember-on-helper/helpers/on-window.d.ts
+++ ember-bootstrap/types/ember-on-helper/helpers/on-window.d.ts
@@ -1,12 +1,8 @@
 import { HelperLike } from '@glint/template';
 
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
-    'on-window': HelperLike<{
-      Args: {
-        Positional: [event: keyof WindowEventMap, listener: () => void];
-      };
-      Return: string;
-    }>;
-  }
-}
+export default HelperLike<{
+  Args: {
+    Positional: [event: keyof WindowEventMap, listener: () => void];
+  };
+  Return: string;
+}>;
